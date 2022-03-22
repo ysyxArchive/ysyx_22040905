dbcaa48b4374a937499f65347b9028a2d36115b7
tracer-ysyx2204 <tracer@ysyx.org>
2022-03-22 17:22:49 +0800

    > sim RTL ysyx_22040905 程雨航 Linux agustin-RedmiBook-Pro-14S 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  17:22:49 up  2:57,  4 users,  load average: 0.13, 0.25, 0.29

---
+++ npc/csrc/1.cpp
@@ -0,0 +1,43 @@
+#include<nvboard.h>
+#include<stdio.h>
+#include<stdlib.h>
+#include<assert.h>
+#include<verilated.h>
+#include<Vtop.h>
+#include<verilated_vcd_c.h>
+#define MAX_SIM_TIME 20
+static TOP_NAME dut;
+
+void nvboard_bind_all_pins(Vtop* top);
+
+vluint64_t sim_time=0;
+int main(int argc,char ** argv, char** env)
+{
+    nvboard_bind_all_pins(&dut);
+	nvboard_init();
+
+	Verilated::traceEverOn(true);
+	VerilatedVcdC* tfp=new VerilatedVcdC;
+
+	Vtop* top =new Vtop;
+	top->trace(tfp,5);
+	tfp->open("wave.vcd");
+
+	while(sim_time<MAX_SIM_TIME){
+	nvboard_update();
+	int a=rand()&1;
+	int b=rand()&1;
+	top->a=a;
+	top->b=b;
+	top->eval();
+	tfp->dump(sim_time);
+	sim_time++;
+	printf("a = %d, b = %d, f = %d\n", a, b, top->f);
+	assert(top->f == a^b);
+	}
+	tfp->close();
+	delete top;
+    nvboard_quit();
+	return 0;
+
+}

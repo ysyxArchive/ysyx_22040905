5d8d0b2904112ca542b0ca8e1a79a0b2a121e5f6
tracer-ysyx2204 <tracer@ysyx.org>
2022-06-10 21:07:39 +0800

    > generate verilog ysyx_22040905 程雨航 Linux agustin-RedmiBook-Pro-14S 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  21:07:39 up 50 min,  1 user,  load average: 0.18, 0.37, 0.35

--- npc/playground/src/Passthrough.scala
+++ npc/playground/src/Passthrough.scala
@@ -1,10 +1,28 @@
 import chisel3._
 import chisel3.util.Decoupled
 
-class Passthrough(width: Int) extends Module { 
+class Passthrough extends Module { 
   val io = IO(new Bundle {
-    val in = Input(UInt(width.W))
-    val out = Output(UInt(width.W))
+    val x0 = Input(UInt(2.W))
+    val x1 = Input(UInt(2.W))
+    val x2 = Input(UInt(2.W))
+    val x3 = Input(UInt(2.W))
+    val y  = Input(UInt(2.W))
+    val F  = Output(UInt(2.W))
   })
-  io.out := io.in
+  switch(y){
+   is(0.U){
+     F := x0
+   }
+   is(1.U){
+     F := x1
+   }
+   is(2.U){
+     F := x2
+   }
+   is(3.U){
+     F := x3
+   }
+
+  }
 }

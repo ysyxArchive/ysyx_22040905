a1c633261e47345b6dee45c105980489c044026d
tracer-ysyx2204 <tracer@ysyx.org>
2022-06-13 00:00:59 +0800

    > sim RTL ysyx_22040905 程雨航 Linux agustin-RedmiBook-Pro-14S 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  00:00:59 up 4 min,  1 user,  load average: 0.36, 0.38, 0.18

--- npc/csrc/Passthrough.cpp
+++ npc/csrc/Passthrough.cpp
@@ -18,14 +18,14 @@ int main(int argc,char ** argv, char** env)
 
 
     while(sim_time<MAX_SIM_TIME){
-    top->io_x0=0,top->io_x1=1,top->io_x2=2,top->io_x3=3;
-    top->io_y=rand()%4;
+    top->io_x0=0,top->io_x1=1,top->io_x2=1,top->io_x3=0;
+    top->io_y=rand()%2;
 
     top->eval();
     tfp->dump(contextp->time());
     contextp->timeInc(1);
     printf("F = %d\n",top->io_F);
-    assert(top->io_F == top->io_y);
+    //assert(top->io_F == top->io_y);
     sim_time++;
     }
     tfp->close();

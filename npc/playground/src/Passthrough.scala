d2b0a758987c594394273232a4fb4846cb2e1228
tracer-ysyx2204 <tracer@ysyx.org>
2022-06-15 00:23:37 +0800

    > generate verilog ysyx_22040905 程雨航 Linux agustin-RedmiBook-Pro-14S 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  00:23:37 up  4:43,  1 user,  load average: 0.71, 0.60, 0.71

--- npc/playground/src/Passthrough.scala
+++ npc/playground/src/Passthrough.scala
@@ -12,44 +12,46 @@ class Passthrough extends Module {
       val outt:UInt =Reg(UInt(3.W))
       io.sign := io.in>0.U 
       when(io.en){
-        for(i <- 0 to 8){
-          when(io.in===(1.U<<i)){
-            outt := UInt(i);
-          }
+        when(io.in&"10000000".U){
+            outt := 7.U
+        }.elsewhen(io.in&"01000000".U){
+            outt := 6.U
+        }.elsewhen(io.in&"00100000".U){
+            outt := 5.U
+        }.elsewhen(io.in&"00010000".U){
+            outt := 4.U
+        }.elsewhen(io.in&"00001000".U){
+            outt := 3.U
+        }.elsewhen(io.in&"00000100".U){
+            outt := 2.U
+        }.elsewhen(io.in&"00000010".U){
+            outt := 1.U
+        }.elsewhen(io.in&"00000001".U){
+            outt := 0.U
         }
         io.out := outt
-        switch(outt){
-          is(0.U){
+        when(outt===0.U){
             io.bcd7seg:= "b1000000".U
-          }i
-          is(1.U){
+        }.elsewhen(outt===1.U){
             io.bcd7seg:= "b1111001".U
-          }
-          is(2.U){
+        }.elsewhen(outt===2.U){
             io.bcd7seg:= "b0100100".U
-          }
-          is(3.U){
+        }.elsewhen(outt===3.U){
             io.bcd7seg:= "b0110000".U
-          }
-          is(4.U){
+        }.elsewhen(outt===4.U){
             io.bcd7seg:= "b0011001".U
-          }
-          is(5.U){
+        }.elsewhen(outt===5.U){
             io.bcd7seg:= "b0010010".U
-          }
-          is(6.U){
+        }.elsewhen(outt===6.U){
             io.bcd7seg:= "b0000010".U
-          }
-          is(7.U){
+        }.elsewhen(outt===7.U){
             io.bcd7seg:= "b1111000".U
-          }
-          is(8.U){
+        }.elsewhen(outt===8.U){
             io.bcd7seg:= "b0000000".U
-          }
-          is(9.U){
+        }.elsewhen(outt===9.U){
             io.bcd7seg:= "b0010000".U
-          }
         }
+        
       }
   }
 

7edb3a75d865b2fad7d584eaa662f4686d07368f
tracer-ysyx2204 <tracer@ysyx.org>
2022-07-06 00:26:50 +0800

    > generate verilog ysyx_22040905 程雨航 Linux agustin-RedmiBook-Pro-14S 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  00:26:50 up  4:45,  1 user,  load average: 0.72, 0.65, 0.60

--- npc/playground/src/Passthrough.scala
+++ npc/playground/src/Passthrough.scala
@@ -5,115 +5,115 @@ class Passthrough extends Module {
   val io = IO(new Bundle {
     val a=Input(UInt(4.W))
     val b=Input(UInt(4.W))
-    val Overflow=Output(bool())
-    val Zero=Output(bool())
+    val Overflow=Output(Bool())
+    val Zero=Output(Bool())
     val Result=Output(UInt(4.W))
-    val Carry=Output(bool())
+    val Carry=Output(Bool())
     val op=Input(UInt(3.W))
     val bcd8seg=Output(UInt(24.W))
   })
    io.bcd8seg:="b000000000000000000000000".U
    io.Overflow:=0
    io.Zero:=1
-   io.Result:=0
+   io.Result:=0.U
    io.Carry:=0
     when(io.op==="b000"){
         io.Result:=io.a+io.b
         io.Overflow:=(io.a(3)===io.b(3))&&(io.Result(3)=/=io.a(3))
         io.Carrry:=((io.a+&io.b)>io.Result)
         io.Zero:=(io.Result===0)
-    }.elsewhen(io.op==="b001"){
+    }.elsewhen(io.op==="b001".U){
         io.Result:=io.a-io.b
         io.Overflow = (io.a(3) === ("b1111"^io.b)(3)) && (io.Result(3) =/= io.a(3))
         io.Carry:=((io.a-&io.b)>io.Result)
         io.Zero:=(io.Result===0)
-    }.elsewhen(io.op==="b010"){
+    }.elsewhen(io.op==="b010".U){
         io.Result:=~io.a
-    }.elsewhen(io.op==="b011"){
+    }.elsewhen(io.op==="b011".U){
         io.Result:=io.a&io.b
-    }.elsewhen(io.op==="b100"){
+    }.elsewhen(io.op==="b100".U){
         io.Result:=io.a|io.b
-    }.elsewhen(io.op==="b101"){
+    }.elsewhen(io.op==="b101".U){
         io.Result:=io.a^io.b
-    }.elsewhen(io.op==="b110"){
+    }.elsewhen(io.op==="b110".U){
         when(io.a<io.b){
-            io.Result:=1
+            io.Result:=1.U
         }
     }.otherwise{
         when(io.a===io.b){
-            io.Result:=1
+            io.Result:=1.U
         }
     }
     when(io.Result%10===0){
-        io.bcd8seg="b000000000000000010000010"^io.bcd8seg
+        io.bcd8seg="b000000000000000010000010".U^io.bcd8seg
     }.elsewhen(io.Result%10===1){
-        io.bcd8seg="b000000000000000010011111"^io.bcd8seg
+        io.bcd8seg="b000000000000000010011111".U^io.bcd8seg
     }.elsewhen(io.Result%10===2){
-        io.bcd8seg="b000000000000000010100101"^io.bcd8seg
+        io.bcd8seg="b000000000000000010100101".U^io.bcd8seg
     }.elsewhen(io.Result%10===3){
-        io.bcd8seg="b000000000000000010001101"^io.bcd8seg
+        io.bcd8seg="b000000000000000010001101".U^io.bcd8seg
     }.elsewhen(io.Result%10===4){
-        io.bcd8seg="b000000000000000010011001"^io.bcd8seg
+        io.bcd8seg="b000000000000000010011001".U^io.bcd8seg
     }.elsewhen(io.Result%10===5){
-        io.bcd8seg="b000000000000000011001001"^io.bcd8seg
+        io.bcd8seg="b000000000000000011001001".U^io.bcd8seg
     }.elsewhen(io.Result%10===6){
-        io.bcd8seg="b000000000000000011000001"^io.bcd8seg
+        io.bcd8seg="b000000000000000011000001".U^io.bcd8seg
     }.elsewhen(io.Result%10===7){
-        io.bcd8seg="b000000000000000010011111"^io.bcd8seg
+        io.bcd8seg="b000000000000000010011111".U^io.bcd8seg
     }.elsewhen(io.Result%10===8){
-        io.bcd8seg="b000000000000000010000000"^io.bcd8seg
+        io.bcd8seg="b000000000000000010000000".U^io.bcd8seg
     }.elsewhen(io.Result%10===9){
-        io.bcd8seg="b000000000000000010010000"^io.bcd8seg
+        io.bcd8seg="b000000000000000010010000".U^io.bcd8seg
     }.otherwise{
-        io.bcd8seg="b000000000000000010111111"^io.bcd8seg
+        io.bcd8seg="b000000000000000010111111".U^io.bcd8seg
     }
 
     when(io.Result/10%10===0){
-        io.bcd8seg="b000000001000001000000000"^io.bcd8seg
+        io.bcd8seg="b000000001000001000000000".U^io.bcd8seg
     }.elsewhen(io.Result/10%10===1){
-        io.bcd8seg="b000000001001111100000000"^io.bcd8seg
+        io.bcd8seg="b000000001001111100000000".U^io.bcd8seg
     }.elsewhen(io.Result/10%10===2){
-        io.bcd8seg="b000000001010010100000000"^io.bcd8seg
+        io.bcd8seg="b000000001010010100000000".U^io.bcd8seg
     }.elsewhen(io.Result/10%10===3){
-        io.bcd8seg="b000000001000110100000000"^io.bcd8seg
+        io.bcd8seg="b000000001000110100000000".U^io.bcd8seg
     }.elsewhen(io.Result/10%10===4){
-        io.bcd8seg="b000000001001100100000000"^io.bcd8seg
+        io.bcd8seg="b000000001001100100000000".U^io.bcd8seg
     }.elsewhen(io.Result/10%10===5){
-        io.bcd8seg="b000000001100100100000000"^io.bcd8seg
+        io.bcd8seg="b000000001100100100000000".U^io.bcd8seg
     }.elsewhen(io.Result/10%10===6){
-        io.bcd8seg="b000000001100000100000000"^io.bcd8seg
+        io.bcd8seg="b000000001100000100000000".U^io.bcd8seg
     }.elsewhen(io.Result/10%10===7){
-        io.bcd8seg="b000000001001111100000000"^io.bcd8seg
+        io.bcd8seg="b000000001001111100000000".U^io.bcd8seg
     }.elsewhen(io.Result/10%10===8){
-        io.bcd8seg="b000000001000000000000000"^io.bcd8seg
+        io.bcd8seg="b000000001000000000000000".U^io.bcd8seg
     }.elsewhen(io.Result/10%10===9){
-        io.bcd8seg="b000000001001000000000000"^io.bcd8seg
+        io.bcd8seg="b000000001001000000000000".U^io.bcd8seg
     }.otherwise{
-        io.bcd8seg="b000000001011111100000000"^io.bcd8seg
+        io.bcd8seg="b000000001011111100000000".U^io.bcd8seg
     }
 
     when(io.Result/100%10===0){
-        io.bcd8seg="b100000100000000000000000"^io.bcd8seg
+        io.bcd8seg="b100000100000000000000000".U^io.bcd8seg
     }.elsewhen(io.Result/100%10===1){
-        io.bcd8seg="b100111110000000000000000"^io.bcd8seg
+        io.bcd8seg="b100111110000000000000000".U^io.bcd8seg
     }.elsewhen(io.Result/100%10===2){
-        io.bcd8seg="b101001010000000000000000"^io.bcd8seg
+        io.bcd8seg="b101001010000000000000000".U^io.bcd8seg
     }.elsewhen(io.Result/100%10===3){
-        io.bcd8seg="b100011010000000000000000"^io.bcd8seg
+        io.bcd8seg="b100011010000000000000000".U^io.bcd8seg
     }.elsewhen(io.Result/100%10===4){
-        io.bcd8seg="b100110010000000000000000"^io.bcd8seg
+        io.bcd8seg="b100110010000000000000000".U^io.bcd8seg
     }.elsewhen(io.Result/100%10===5){
-        io.bcd8seg="b110010010000000000000000"^io.bcd8seg
+        io.bcd8seg="b110010010000000000000000".U^io.bcd8seg
     }.elsewhen(io.Result/100%10===6){
-        io.bcd8seg="b110000010000000000000000"^io.bcd8seg
+        io.bcd8seg="b110000010000000000000000".U^io.bcd8seg
     }.elsewhen(io.Result/100%10===7){
-        io.bcd8seg="b100111110000000000000000"^io.bcd8seg
+        io.bcd8seg="b100111110000000000000000".U^io.bcd8seg
     }.elsewhen(io.Result/100%10===8){
-        io.bcd8seg="b100000000000000000000000"^io.bcd8seg
+        io.bcd8seg="b100000000000000000000000".U^io.bcd8seg
     }.elsewhen(io.Result/100%10===9){
-        io.bcd8seg="b100100000000000000000000"^io.bcd8seg
+        io.bcd8seg="b100100000000000000000000".U^io.bcd8seg
     }.otherwise{
-        io.bcd8seg="b101111110000000000000000"^io.bcd8seg
+        io.bcd8seg="b101111110000000000000000".U^io.bcd8seg
     }
 }
 

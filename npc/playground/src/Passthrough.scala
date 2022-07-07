455a9ac3e2a17a6b4367b97e03caf9ce5998275d
tracer-ysyx2204 <tracer@ysyx.org>
2022-07-07 22:18:22 +0800

    > generate verilog ysyx_22040905 程雨航 Linux agustin-RedmiBook-Pro-14S 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  22:18:22 up  4:40,  1 user,  load average: 0.52, 0.69, 0.64

--- npc/playground/src/Passthrough.scala
+++ npc/playground/src/Passthrough.scala
@@ -3,7 +3,6 @@ import chisel3.util._
 import chisel3.stage._
 class Passthrough extends Module { 
   val io = IO(new Bundle {
-    val button=Input(Clock())
     val bcd8seg=Output(Vec(2,UInt(8.W)))
     val Result=Output(UInt(8.W))
   })
@@ -11,21 +10,16 @@ class Passthrough extends Module {
     val num=RegInit(UInt(8.W),1.U)
     val m1=Module(new seg)
     val m2=Module(new seg)
+    num:=Cat(num(0)^num(1)^num(2)^num(3),num(7,1))
+    when(num===0.U){
+    num:=1.U
+    }
     m1.io.in:=num(3,0)
     m2.io.in:=num(7,4)
     io.bcd8seg(0):=m1.io.out
     io.bcd8seg(1):=m2.io.out
-    withClock (io.button){
-        num:=Cat(num(0)^num(1)^num(2)^num(3),num(7,1))
-        when(num===0.U){
-        num:=1.U
-        }
-        m1.io.in:=num(3,0)
-        m2.io.in:=num(7,4)
-        io.bcd8seg(0):=m1.io.out
-        io.bcd8seg(1):=m2.io.out
-        io.Result:=num
-    }}
+    io.Result:=num
+}}
 class seg extends Module{
     val io= IO(new Bundle{
         val in=Input(UInt(4.W))
--- npc/vsrc/Passthrough.v
+++ npc/vsrc/Passthrough.v
@@ -2,22 +2,22 @@ module seg(
   input  [3:0] io_in,
   output [7:0] io_out
 );
-  wire [7:0] _GEN_0 = io_in == 4'hf ? 8'h8e : 8'hff; // @[Passthrough.scala 61:29 Passthrough.scala 62:15 Passthrough.scala 64:15]
-  wire [7:0] _GEN_1 = io_in == 4'he ? 8'h86 : _GEN_0; // @[Passthrough.scala 59:29 Passthrough.scala 60:15]
-  wire [7:0] _GEN_2 = io_in == 4'hd ? 8'ha1 : _GEN_1; // @[Passthrough.scala 57:29 Passthrough.scala 58:15]
-  wire [7:0] _GEN_3 = io_in == 4'hc ? 8'hc6 : _GEN_2; // @[Passthrough.scala 55:29 Passthrough.scala 56:15]
-  wire [7:0] _GEN_4 = io_in == 4'hb ? 8'h83 : _GEN_3; // @[Passthrough.scala 53:29 Passthrough.scala 54:15]
-  wire [7:0] _GEN_5 = io_in == 4'ha ? 8'h88 : _GEN_4; // @[Passthrough.scala 51:29 Passthrough.scala 52:15]
-  wire [7:0] _GEN_6 = io_in == 4'h9 ? 8'h90 : _GEN_5; // @[Passthrough.scala 49:28 Passthrough.scala 50:15]
-  wire [7:0] _GEN_7 = io_in == 4'h8 ? 8'h80 : _GEN_6; // @[Passthrough.scala 47:28 Passthrough.scala 48:15]
-  wire [7:0] _GEN_8 = io_in == 4'h7 ? 8'hf8 : _GEN_7; // @[Passthrough.scala 45:28 Passthrough.scala 46:15]
-  wire [7:0] _GEN_9 = io_in == 4'h6 ? 8'h82 : _GEN_8; // @[Passthrough.scala 43:28 Passthrough.scala 44:15]
-  wire [7:0] _GEN_10 = io_in == 4'h5 ? 8'h92 : _GEN_9; // @[Passthrough.scala 41:28 Passthrough.scala 42:15]
-  wire [7:0] _GEN_11 = io_in == 4'h4 ? 8'h99 : _GEN_10; // @[Passthrough.scala 39:28 Passthrough.scala 40:15]
-  wire [7:0] _GEN_12 = io_in == 4'h3 ? 8'hb0 : _GEN_11; // @[Passthrough.scala 37:28 Passthrough.scala 38:15]
-  wire [7:0] _GEN_13 = io_in == 4'h2 ? 8'ha4 : _GEN_12; // @[Passthrough.scala 35:28 Passthrough.scala 36:15]
-  wire [7:0] _GEN_14 = io_in == 4'h1 ? 8'hf9 : _GEN_13; // @[Passthrough.scala 33:28 Passthrough.scala 34:15]
-  assign io_out = io_in == 4'h0 ? 8'hc0 : _GEN_14; // @[Passthrough.scala 31:22 Passthrough.scala 32:15]
+  wire [7:0] _GEN_0 = io_in == 4'hf ? 8'h8e : 8'hff; // @[Passthrough.scala 65:29 Passthrough.scala 66:15 Passthrough.scala 68:15]
+  wire [7:0] _GEN_1 = io_in == 4'he ? 8'h86 : _GEN_0; // @[Passthrough.scala 63:29 Passthrough.scala 64:15]
+  wire [7:0] _GEN_2 = io_in == 4'hd ? 8'ha1 : _GEN_1; // @[Passthrough.scala 61:29 Passthrough.scala 62:15]
+  wire [7:0] _GEN_3 = io_in == 4'hc ? 8'hc6 : _GEN_2; // @[Passthrough.scala 59:29 Passthrough.scala 60:15]
+  wire [7:0] _GEN_4 = io_in == 4'hb ? 8'h83 : _GEN_3; // @[Passthrough.scala 57:29 Passthrough.scala 58:15]
+  wire [7:0] _GEN_5 = io_in == 4'ha ? 8'h88 : _GEN_4; // @[Passthrough.scala 55:29 Passthrough.scala 56:15]
+  wire [7:0] _GEN_6 = io_in == 4'h9 ? 8'h90 : _GEN_5; // @[Passthrough.scala 53:28 Passthrough.scala 54:15]
+  wire [7:0] _GEN_7 = io_in == 4'h8 ? 8'h80 : _GEN_6; // @[Passthrough.scala 51:28 Passthrough.scala 52:15]
+  wire [7:0] _GEN_8 = io_in == 4'h7 ? 8'hf8 : _GEN_7; // @[Passthrough.scala 49:28 Passthrough.scala 50:15]
+  wire [7:0] _GEN_9 = io_in == 4'h6 ? 8'h82 : _GEN_8; // @[Passthrough.scala 47:28 Passthrough.scala 48:15]
+  wire [7:0] _GEN_10 = io_in == 4'h5 ? 8'h92 : _GEN_9; // @[Passthrough.scala 45:28 Passthrough.scala 46:15]
+  wire [7:0] _GEN_11 = io_in == 4'h4 ? 8'h99 : _GEN_10; // @[Passthrough.scala 43:28 Passthrough.scala 44:15]
+  wire [7:0] _GEN_12 = io_in == 4'h3 ? 8'hb0 : _GEN_11; // @[Passthrough.scala 41:28 Passthrough.scala 42:15]
+  wire [7:0] _GEN_13 = io_in == 4'h2 ? 8'ha4 : _GEN_12; // @[Passthrough.scala 39:28 Passthrough.scala 40:15]
+  wire [7:0] _GEN_14 = io_in == 4'h1 ? 8'hf9 : _GEN_13; // @[Passthrough.scala 37:28 Passthrough.scala 38:15]
+  assign io_out = io_in == 4'h0 ? 8'hc0 : _GEN_14; // @[Passthrough.scala 35:22 Passthrough.scala 36:15]
 endmodule
 module Passthrough(
   input        clock,
@@ -35,8 +35,8 @@ module Passthrough(
   wire [3:0] m2_io_in; // @[Passthrough.scala 13:18]
   wire [7:0] m2_io_out; // @[Passthrough.scala 13:18]
   reg [7:0] num; // @[Passthrough.scala 11:20]
-  wire  num_hi = num[0] ^ num[1] ^ num[2] ^ num[3]; // @[Passthrough.scala 15:38]
-  wire [6:0] num_lo = num[7:1]; // @[Passthrough.scala 15:49]
+  wire  num_hi = num[0] ^ num[1] ^ num[2] ^ num[3]; // @[Passthrough.scala 19:38]
+  wire [6:0] num_lo = num[7:1]; // @[Passthrough.scala 19:49]
   wire [7:0] _num_T_6 = {num_hi,num_lo}; // @[Cat.scala 30:58]
   seg m1 ( // @[Passthrough.scala 12:18]
     .io_in(m1_io_in),
@@ -46,18 +46,18 @@ module Passthrough(
     .io_in(m2_io_in),
     .io_out(m2_io_out)
   );
-  assign io_bcd8seg_0 = m1_io_out; // @[Passthrough.scala 21:18]
-  assign io_bcd8seg_1 = m2_io_out; // @[Passthrough.scala 22:18]
-  assign io_Result = num; // @[Passthrough.scala 23:14]
-  assign m1_io_in = num[3:0]; // @[Passthrough.scala 19:18]
-  assign m2_io_in = num[7:4]; // @[Passthrough.scala 20:18]
+  assign io_bcd8seg_0 = m1_io_out; // @[Passthrough.scala 25:22]
+  assign io_bcd8seg_1 = m2_io_out; // @[Passthrough.scala 26:22]
+  assign io_Result = num; // @[Passthrough.scala 27:18]
+  assign m1_io_in = num[3:0]; // @[Passthrough.scala 23:22]
+  assign m2_io_in = num[7:4]; // @[Passthrough.scala 24:22]
   always @(posedge clock) begin
     if (reset) begin // @[Passthrough.scala 11:20]
       num <= 8'h1; // @[Passthrough.scala 11:20]
-    end else if (num == 8'h0) begin // @[Passthrough.scala 16:24]
-      num <= 8'h1; // @[Passthrough.scala 17:12]
+    end else if (num == 8'h0) begin // @[Passthrough.scala 20:24]
+      num <= 8'h1; // @[Passthrough.scala 21:12]
     end else begin
-      num <= _num_T_6; // @[Passthrough.scala 15:12]
+      num <= _num_T_6; // @[Passthrough.scala 19:12]
     end
   end
 // Register and memory initialization

416018a28652231957b9ae9eba09d586a779988e
tracer-ysyx2204 <tracer@ysyx.org>
2022-07-07 21:43:24 +0800

    > generate verilog ysyx_22040905 程雨航 Linux agustin-RedmiBook-Pro-14S 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  21:43:24 up  4:05,  1 user,  load average: 0.92, 0.58, 0.50

--- npc/playground/src/Passthrough.scala
+++ npc/playground/src/Passthrough.scala
@@ -3,7 +3,7 @@ import chisel3.util._
 import chisel3.stage._
 class Passthrough extends Module { 
   val io = IO(new Bundle {
-    val button=Input(Bool())
+    val button=Input(Clock())
     val bcd8seg=Output(Vec(2,UInt(8.W)))
     val Result=Output(UInt(8.W))
   })

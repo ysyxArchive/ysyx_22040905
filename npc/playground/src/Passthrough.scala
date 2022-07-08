0347e1fc93e4933fb49ade8f1eac48ea46eaa977
tracer-ysyx2204 <tracer@ysyx.org>
2022-07-08 22:50:26 +0800

    > generate verilog ysyx_22040905 程雨航 Linux agustin-RedmiBook-Pro-14S 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  22:50:26 up  7:10,  1 user,  load average: 0.63, 0.75, 0.70

--- npc/playground/src/Passthrough.scala
+++ npc/playground/src/Passthrough.scala
@@ -13,7 +13,7 @@ class Passthrough extends Module {
     })
     val ov=Reg(Bool())
     ov:=false.B
-    val buffer=Vec(10,0.U(1.W))
+    val buffer=Vec(10,UInt(1.W))
     val fifo=Vec(8,0.U(8.W))
     val w_ptr=RegInit(0.U(3.W))
     val r_ptr=RegInit(0.U(3.W))

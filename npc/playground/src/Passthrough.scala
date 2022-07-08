46d310053e976ca1da98978272fc744a05d529ae
tracer-ysyx2204 <tracer@ysyx.org>
2022-07-08 22:23:26 +0800

    > generate verilog ysyx_22040905 程雨航 Linux agustin-RedmiBook-Pro-14S 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  22:23:26 up  6:43,  1 user,  load average: 0.19, 0.34, 0.38

--- npc/playground/src/Passthrough.scala
+++ npc/playground/src/Passthrough.scala
@@ -46,8 +46,8 @@ class Passthrough extends Module {
                 }
                 count:=0.U
             }.elsewhen{
-                buffer(count):=io.ps2_data.asBool()
-                //count:=count+1.U
+                buffer(1):=io.ps2_data
+                count:=count+1.U
             }
         }
     }

bb067abb058c747cba058e646184704e5f31e0b1
tracer-ysyx2204 <tracer@ysyx.org>
2022-07-09 21:37:42 +0800

    > generate verilog ysyx_22040905 程雨航 Linux agustin-RedmiBook-Pro-14S 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  21:37:42 up  4:18,  1 user,  load average: 0.99, 1.37, 1.10

--- npc/playground/src/Passthrough.scala
+++ npc/playground/src/Passthrough.scala
@@ -3,7 +3,7 @@ import chisel3.util._
 import chisel3.stage._
 class Passthrough extends Module{
     val io= IO(new Bundle{
-        val reset=Input(Reset())
+        val reset=Input(UInt(1.W))
         val ps2_clk=Input(UInt(1.W))
         val ps2_data=Input(UInt(1.W))
         val bcd8seg=Output(Vec(8,UInt(8.W)))
@@ -13,7 +13,7 @@ class Passthrough extends Module{
     val overflow=Reg(UInt(1.W))
     val nextdata=Reg(UInt(1.W))
     val ps2=Module(new ps2_keyboard)
-    ps2.io.clrn:=(~reset.asBool)
+    ps2.io.clrn:=(~reset)
     ps2.io.ps2_clk:=io.ps2_clk
     ps2.io.ps2_data:=io.ps2_data
     ps2.io.nextdata_n:=nextdata

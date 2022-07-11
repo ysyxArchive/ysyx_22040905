6d9719f1075640c820ab4651ea7a3ec04031e8f1
tracer-ysyx2204 <tracer@ysyx.org>
2022-07-11 23:00:49 +0800

    > generate verilog ysyx_22040905 程雨航 Linux agustin-RedmiBook-Pro-14S 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  23:00:49 up  9:16,  1 user,  load average: 0.27, 0.33, 0.49

--- npc/playground/src/vga/vga.scala
+++ npc/playground/src/vga/vga.scala
@@ -43,193 +43,26 @@ class vmem extends Module{
         val v_addr=Input(UInt(9.W))
         val vga_data=Output(UInt(24.W))
     })
-    val ram=RegInit(VecInit(Seq.fill(16*200)(0.U(1.W)))) 
+    val ram=RegInit(VecInit(Seq.fill(16)(0.U(90.W)))) 
     val h=RegInit(0.U(10.W))
+    h:=0.U
     val v=RegInit(0.U(9.W))
     val memoryFile: String = "resource/vga_font.txt"
     val vga_mem = SyncReadMem(4096, UInt(12.W))
     if (memoryFile.trim().nonEmpty) {
         loadMemoryFromFileInline(vga_mem,memoryFile)
     }
-    when(io.now=/=0.U){
-        ram(h*(v+0.U)):=vga_mem(16.U*io.ascii)(0)
-        ram(h*(v+1.U)):=vga_mem(16.U*io.ascii)(1)
-        ram(h*(v+2.U)):=vga_mem(16.U*io.ascii)(2)
-        ram(h*(v+3.U)):=vga_mem(16.U*io.ascii)(3)
-        ram(h*(v+4.U)):=vga_mem(16.U*io.ascii)(4)
-        ram(h*(v+5.U)):=vga_mem(16.U*io.ascii)(5)
-        ram(h*(v+6.U)):=vga_mem(16.U*io.ascii)(6)
-        ram(h*(v+7.U)):=vga_mem(16.U*io.ascii)(7)
-        ram(h*(v+8.U)):=vga_mem(16.U*io.ascii)(8)
-        
-        ram((h+1.U)*(v+0.U)):=vga_mem(16.U*io.ascii+1.U)(0)
-        ram((h+1.U)*(v+1.U)):=vga_mem(16.U*io.ascii+1.U)(1)
-        ram((h+1.U)*(v+2.U)):=vga_mem(16.U*io.ascii+1.U)(2)
-        ram((h+1.U)*(v+3.U)):=vga_mem(16.U*io.ascii+1.U)(3)
-        ram((h+1.U)*(v+4.U)):=vga_mem(16.U*io.ascii+1.U)(4)
-        ram((h+1.U)*(v+5.U)):=vga_mem(16.U*io.ascii+1.U)(5)
-        ram((h+1.U)*(v+6.U)):=vga_mem(16.U*io.ascii+1.U)(6)
-        ram((h+1.U)*(v+7.U)):=vga_mem(16.U*io.ascii+1.U)(7)
-        ram((h+1.U)*(v+8.U)):=vga_mem(16.U*io.ascii+1.U)(8)
-        
-
-        ram((h+2.U)*(v+0.U)):=vga_mem(16.U*io.ascii+2.U)(0)
-        ram((h+2.U)*(v+1.U)):=vga_mem(16.U*io.ascii+2.U)(1)
-        ram((h+2.U)*(v+2.U)):=vga_mem(16.U*io.ascii+2.U)(2)
-        ram((h+2.U)*(v+3.U)):=vga_mem(16.U*io.ascii+2.U)(3)
-        ram((h+2.U)*(v+4.U)):=vga_mem(16.U*io.ascii+2.U)(4)
-        ram((h+2.U)*(v+5.U)):=vga_mem(16.U*io.ascii+2.U)(5)
-        ram((h+2.U)*(v+6.U)):=vga_mem(16.U*io.ascii+2.U)(6)
-        ram((h+2.U)*(v+7.U)):=vga_mem(16.U*io.ascii+2.U)(7)
-        ram((h+2.U)*(v+8.U)):=vga_mem(16.U*io.ascii+2.U)(8)
-        
-
-        ram((h+3.U)*(v+0.U)):=vga_mem(16.U*io.ascii+3.U)(0)
-        ram((h+3.U)*(v+1.U)):=vga_mem(16.U*io.ascii+3.U)(1)
-        ram((h+3.U)*(v+2.U)):=vga_mem(16.U*io.ascii+3.U)(2)
-        ram((h+3.U)*(v+3.U)):=vga_mem(16.U*io.ascii+3.U)(3)
-        ram((h+3.U)*(v+4.U)):=vga_mem(16.U*io.ascii+3.U)(4)
-        ram((h+3.U)*(v+5.U)):=vga_mem(16.U*io.ascii+3.U)(5)
-        ram((h+3.U)*(v+6.U)):=vga_mem(16.U*io.ascii+3.U)(6)
-        ram((h+3.U)*(v+7.U)):=vga_mem(16.U*io.ascii+3.U)(7)
-        ram((h+3.U)*(v+8.U)):=vga_mem(16.U*io.ascii+3.U)(8)
-        
-
-        ram((h+4.U)*(v+0.U)):=vga_mem(16.U*io.ascii+4.U)(0)
-        ram((h+4.U)*(v+1.U)):=vga_mem(16.U*io.ascii+4.U)(1)
-        ram((h+4.U)*(v+2.U)):=vga_mem(16.U*io.ascii+4.U)(2)
-        ram((h+4.U)*(v+3.U)):=vga_mem(16.U*io.ascii+4.U)(3)
-        ram((h+4.U)*(v+4.U)):=vga_mem(16.U*io.ascii+4.U)(4)
-        ram((h+4.U)*(v+5.U)):=vga_mem(16.U*io.ascii+4.U)(5)
-        ram((h+4.U)*(v+6.U)):=vga_mem(16.U*io.ascii+4.U)(6)
-        ram((h+4.U)*(v+7.U)):=vga_mem(16.U*io.ascii+4.U)(7)
-        ram((h+4.U)*(v+8.U)):=vga_mem(16.U*io.ascii+4.U)(8)
-        
-
-        ram((h+5.U)*(v+0.U)):=vga_mem(16.U*io.ascii+5.U)(0)
-        ram((h+5.U)*(v+1.U)):=vga_mem(16.U*io.ascii+5.U)(1)
-        ram((h+5.U)*(v+2.U)):=vga_mem(16.U*io.ascii+5.U)(2)
-        ram((h+5.U)*(v+3.U)):=vga_mem(16.U*io.ascii+5.U)(3)
-        ram((h+5.U)*(v+4.U)):=vga_mem(16.U*io.ascii+5.U)(4)
-        ram((h+5.U)*(v+5.U)):=vga_mem(16.U*io.ascii+5.U)(5)
-        ram((h+5.U)*(v+6.U)):=vga_mem(16.U*io.ascii+5.U)(6)
-        ram((h+5.U)*(v+7.U)):=vga_mem(16.U*io.ascii+5.U)(7)
-        ram((h+5.U)*(v+8.U)):=vga_mem(16.U*io.ascii+5.U)(8)
-        
-
-        ram((h+6.U)*(v+0.U)):=vga_mem(16.U*io.ascii+6.U)(0)
-        ram((h+6.U)*(v+1.U)):=vga_mem(16.U*io.ascii+6.U)(1)
-        ram((h+6.U)*(v+2.U)):=vga_mem(16.U*io.ascii+6.U)(2)
-        ram((h+6.U)*(v+3.U)):=vga_mem(16.U*io.ascii+6.U)(3)
-        ram((h+6.U)*(v+4.U)):=vga_mem(16.U*io.ascii+6.U)(4)
-        ram((h+6.U)*(v+5.U)):=vga_mem(16.U*io.ascii+6.U)(5)
-        ram((h+6.U)*(v+6.U)):=vga_mem(16.U*io.ascii+6.U)(6)
-        ram((h+6.U)*(v+7.U)):=vga_mem(16.U*io.ascii+6.U)(7)
-        ram((h+6.U)*(v+8.U)):=vga_mem(16.U*io.ascii+6.U)(8)
-        
-
-        ram((h+7.U)*(v+0.U)):=vga_mem(16.U*io.ascii+7.U)(0)
-        ram((h+7.U)*(v+1.U)):=vga_mem(16.U*io.ascii+7.U)(1)
-        ram((h+7.U)*(v+2.U)):=vga_mem(16.U*io.ascii+7.U)(2)
-        ram((h+7.U)*(v+3.U)):=vga_mem(16.U*io.ascii+7.U)(3)
-        ram((h+7.U)*(v+4.U)):=vga_mem(16.U*io.ascii+7.U)(4)
-        ram((h+7.U)*(v+5.U)):=vga_mem(16.U*io.ascii+7.U)(5)
-        ram((h+7.U)*(v+6.U)):=vga_mem(16.U*io.ascii+7.U)(6)
-        ram((h+7.U)*(v+7.U)):=vga_mem(16.U*io.ascii+7.U)(7)
-        ram((h+7.U)*(v+8.U)):=vga_mem(16.U*io.ascii+7.U)(8)
-        
-
-        ram((h+8.U)*(v+0.U)):=vga_mem(16.U*io.ascii+8.U)(0)
-        ram((h+8.U)*(v+1.U)):=vga_mem(16.U*io.ascii+8.U)(1)
-        ram((h+8.U)*(v+2.U)):=vga_mem(16.U*io.ascii+8.U)(2)
-        ram((h+8.U)*(v+3.U)):=vga_mem(16.U*io.ascii+8.U)(3)
-        ram((h+8.U)*(v+4.U)):=vga_mem(16.U*io.ascii+8.U)(4)
-        ram((h+8.U)*(v+5.U)):=vga_mem(16.U*io.ascii+8.U)(5)
-        ram((h+8.U)*(v+6.U)):=vga_mem(16.U*io.ascii+8.U)(6)
-        ram((h+8.U)*(v+7.U)):=vga_mem(16.U*io.ascii+8.U)(7)
-        ram((h+8.U)*(v+8.U)):=vga_mem(16.U*io.ascii+8.U)(8)
-        
-
-        ram((h+9.U)*(v+0.U)):=vga_mem(16.U*io.ascii+9.U)(0)
-        ram((h+9.U)*(v+1.U)):=vga_mem(16.U*io.ascii+9.U)(1)
-        ram((h+9.U)*(v+2.U)):=vga_mem(16.U*io.ascii+9.U)(2)
-        ram((h+9.U)*(v+3.U)):=vga_mem(16.U*io.ascii+9.U)(3)
-        ram((h+9.U)*(v+4.U)):=vga_mem(16.U*io.ascii+9.U)(4)
-        ram((h+9.U)*(v+5.U)):=vga_mem(16.U*io.ascii+9.U)(5)
-        ram((h+9.U)*(v+6.U)):=vga_mem(16.U*io.ascii+9.U)(6)
-        ram((h+9.U)*(v+7.U)):=vga_mem(16.U*io.ascii+9.U)(7)
-        ram((h+9.U)*(v+8.U)):=vga_mem(16.U*io.ascii+9.U)(8)
-        
-
-        ram((h+10.U)*(v+0.U)):=vga_mem(16.U*io.ascii+10.U)(0)
-        ram((h+10.U)*(v+1.U)):=vga_mem(16.U*io.ascii+10.U)(1)
-        ram((h+10.U)*(v+2.U)):=vga_mem(16.U*io.ascii+10.U)(2)
-        ram((h+10.U)*(v+3.U)):=vga_mem(16.U*io.ascii+10.U)(3)
-        ram((h+10.U)*(v+4.U)):=vga_mem(16.U*io.ascii+10.U)(4)
-        ram((h+10.U)*(v+5.U)):=vga_mem(16.U*io.ascii+10.U)(5)
-        ram((h+10.U)*(v+6.U)):=vga_mem(16.U*io.ascii+10.U)(6)
-        ram((h+10.U)*(v+7.U)):=vga_mem(16.U*io.ascii+10.U)(7)
-        ram((h+10.U)*(v+8.U)):=vga_mem(16.U*io.ascii+10.U)(8)
-        
-
-        ram((h+11.U)*(v+0.U)):=vga_mem(16.U*io.ascii+11.U)(0)
-        ram((h+11.U)*(v+1.U)):=vga_mem(16.U*io.ascii+11.U)(1)
-        ram((h+11.U)*(v+2.U)):=vga_mem(16.U*io.ascii+11.U)(2)
-        ram((h+11.U)*(v+3.U)):=vga_mem(16.U*io.ascii+11.U)(3)
-        ram((h+11.U)*(v+4.U)):=vga_mem(16.U*io.ascii+11.U)(4)
-        ram((h+11.U)*(v+5.U)):=vga_mem(16.U*io.ascii+11.U)(5)
-        ram((h+11.U)*(v+6.U)):=vga_mem(16.U*io.ascii+11.U)(6)
-        ram((h+11.U)*(v+7.U)):=vga_mem(16.U*io.ascii+11.U)(7)
-        ram((h+11.U)*(v+8.U)):=vga_mem(16.U*io.ascii+11.U)(8)
-        
-
-        ram((h+12.U)*(v+0.U)):=vga_mem(16.U*io.ascii+12.U)(0)
-        ram((h+12.U)*(v+1.U)):=vga_mem(16.U*io.ascii+12.U)(1)
-        ram((h+12.U)*(v+2.U)):=vga_mem(16.U*io.ascii+12.U)(2)
-        ram((h+12.U)*(v+3.U)):=vga_mem(16.U*io.ascii+12.U)(3)
-        ram((h+12.U)*(v+4.U)):=vga_mem(16.U*io.ascii+12.U)(4)
-        ram((h+12.U)*(v+5.U)):=vga_mem(16.U*io.ascii+12.U)(5)
-        ram((h+12.U)*(v+6.U)):=vga_mem(16.U*io.ascii+12.U)(6)
-        ram((h+12.U)*(v+7.U)):=vga_mem(16.U*io.ascii+12.U)(7)
-        ram((h+12.U)*(v+8.U)):=vga_mem(16.U*io.ascii+12.U)(8)
-        
-
-        ram((h+13.U)*(v+0.U)):=vga_mem(16.U*io.ascii+13.U)(0)
-        ram((h+13.U)*(v+1.U)):=vga_mem(16.U*io.ascii+13.U)(1)
-        ram((h+13.U)*(v+2.U)):=vga_mem(16.U*io.ascii+13.U)(2)
-        ram((h+13.U)*(v+3.U)):=vga_mem(16.U*io.ascii+13.U)(3)
-        ram((h+13.U)*(v+4.U)):=vga_mem(16.U*io.ascii+13.U)(4)
-        ram((h+13.U)*(v+5.U)):=vga_mem(16.U*io.ascii+13.U)(5)
-        ram((h+13.U)*(v+6.U)):=vga_mem(16.U*io.ascii+13.U)(6)
-        ram((h+13.U)*(v+7.U)):=vga_mem(16.U*io.ascii+13.U)(7)
-        ram((h+13.U)*(v+8.U)):=vga_mem(16.U*io.ascii+13.U)(8)
-        
-
-        ram((h+14.U)*(v+0.U)):=vga_mem(16.U*io.ascii+14.U)(0)
-        ram((h+14.U)*(v+1.U)):=vga_mem(16.U*io.ascii+14.U)(1)
-        ram((h+14.U)*(v+2.U)):=vga_mem(16.U*io.ascii+14.U)(2)
-        ram((h+14.U)*(v+3.U)):=vga_mem(16.U*io.ascii+14.U)(3)
-        ram((h+14.U)*(v+4.U)):=vga_mem(16.U*io.ascii+14.U)(4)
-        ram((h+14.U)*(v+5.U)):=vga_mem(16.U*io.ascii+14.U)(5)
-        ram((h+14.U)*(v+6.U)):=vga_mem(16.U*io.ascii+14.U)(6)
-        ram((h+14.U)*(v+7.U)):=vga_mem(16.U*io.ascii+14.U)(7)
-        ram((h+14.U)*(v+8.U)):=vga_mem(16.U*io.ascii+14.U)(8)
-        
-
-        ram((h+15.U)*(v+0.U)):=vga_mem(16.U*io.ascii+15.U)(0)
-        ram((h+15.U)*(v+1.U)):=vga_mem(16.U*io.ascii+15.U)(1)
-        ram((h+15.U)*(v+2.U)):=vga_mem(16.U*io.ascii+15.U)(2)
-        ram((h+15.U)*(v+3.U)):=vga_mem(16.U*io.ascii+15.U)(3)
-        ram((h+15.U)*(v+4.U)):=vga_mem(16.U*io.ascii+15.U)(4)
-        ram((h+15.U)*(v+5.U)):=vga_mem(16.U*io.ascii+15.U)(5)
-        ram((h+15.U)*(v+6.U)):=vga_mem(16.U*io.ascii+15.U)(6)
-        ram((h+15.U)*(v+7.U)):=vga_mem(16.U*io.ascii+15.U)(7)
-        ram((h+15.U)*(v+8.U)):=vga_mem(16.U*io.ascii+15.U)(8)
-
-        v:=v+9.U        
+    when(v>=90.U){
+        io.now:=0.U
+    }
+    when(io.now=/=0.U){         
+        for (i <- 0 to 15){
+        ram(h.asSInt+i):=ram(h.asSInt+i)^Cat(Cat(Fill(v*9.U,0.U),vga_mem(16.U*io.ascii)(0,8),Fill(90.U-v*9.U-9.U,0.U)))
+        }
+        v:=v+9.U
     }
     val rdwrPort = Reg(UInt(24.W))
-    when(ram(io.h_addr*io.v_addr)===1.U){
+    when(ram(io.h_addr)(io.v_addr)===1.U){
         rdwrPort:=Fill(24,1.U)
     }.otherwise{
         rdwrPort:=Fill(24,0.U)

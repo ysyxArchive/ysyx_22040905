f6f63ca7533f627b59dd53982e4597de039a6084
tracer-ysyx2204 <tracer@ysyx.org>
2022-07-14 01:11:22 +0800

    > compile NEMU ysyx_22040905 程雨航 Linux agustin-RedmiBook-Pro-14S 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  01:11:22 up  5:27,  1 user,  load average: 0.59, 0.87, 0.90

--- nemu/src/isa/riscv64/reg.c
+++ nemu/src/isa/riscv64/reg.c
@@ -10,7 +10,7 @@ const char *regs[] = {
 
 void isa_reg_display() {
   for(int i=0;i<32;i++){
-    printf("%s\t\t%ld\n",reg_name(i,10),gpr(i));
+    printf("%s\t\t%ld\n",reg_name(i,8),gpr(i));
   }
 }
 

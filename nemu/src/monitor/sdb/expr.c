fd54e01ce491849b47b220f97c462ab492f7a57a
tracer-ysyx2204 <tracer@ysyx.org>
2022-07-18 16:50:37 +0800

    > compile NEMU ysyx_22040905 程雨航 Linux agustin-RedmiBook-Pro-14S 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  16:50:37 up 38 min,  1 user,  load average: 0.15, 0.34, 0.42

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -181,7 +181,7 @@ uint32_t eval (int p,int q){
     printf("%d\n",op);
     uint32_t val1=eval(p,op-1);
     uint32_t val2=eval(op+1,q);
-    printf("%d %d %d\n",p,op,q);
+    printf("%s %d\n",tokens[2].str,atoi(tokens[2].str));
     switch (tokens[op].type){
       case '+': return val1+val2;
       case '-': return val1-val2;

43624d8675a3a58a92110f1017b79a98103d325b
tracer-ysyx2204 <tracer@ysyx.org>
2022-07-24 21:45:49 +0800

    > compile NEMU ysyx_22040905 程雨航 Linux agustin-RedmiBook-Pro-14S 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  21:45:49 up  1:40,  1 user,  load average: 0.37, 0.42, 0.43

--- nemu/src/isa/riscv64/inst.c
+++ nemu/src/isa/riscv64/inst.c
@@ -36,7 +36,7 @@ static void decode_operand(Decode *s, word_t *dest, word_t *src1, word_t *src2, 
     case TYPE_U: src1I(immU(i)); break;
     case TYPE_S: destI(immS(i)); src1R(rs1); src2R(rs2); break;
     case TYPE_J: src1I(immJ(i));break;
-    case TYPE_B: destI(immB(i)); printf("%x\n\n",i);src1R(rs1); src2R(rs2); break;
+    case TYPE_B: printf("%x\n\n",i);destI(immB(i)); src1R(rs1); src2R(rs2); break;
     case TYPE_R: src1R(rs1); src2R(rs2); break;
   }
 }

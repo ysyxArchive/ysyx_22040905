0389758234f5daaf30bb330cc9d9b95796146ecc
tracer-ysyx2204 <tracer@ysyx.org>
2022-08-22 22:44:16 +0800

    > compile NEMU ysyx_22040905 程雨航 Linux agustin-RedmiBook-Pro-14S 5.15.0-25-generic #25-Ubuntu SMP Wed Mar 30 15:54:22 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  22:44:16 up  4:12,  2 users,  load average: 1.73, 0.85, 0.70

--- npc/csrc/memory/memory.c
+++ npc/csrc/memory/memory.c
@@ -1,84 +1,35 @@
-#include"../include/macro.h"
-static uint8_t pmem[CONFIG_MSIZE];
-
-static inline word_t host_read(void *addr, int len) {
-  switch (len) {
-    case 1: return *(uint8_t  *)addr;
-    case 2: return *(uint16_t *)addr;
-    case 4: return *(uint32_t *)addr;
-    case 8: return *(uint64_t *)addr;
-    default: assert(0);
-  }
+#define pmem_size 32768
+u_int32_t pmem_inst[pmem_size];
+u_int64_t  pmem_data[pmem_size];
+u_int32_t pmem_inst_read(u_int64_t pc){
+  return pmem_inst[(pc-0x80000000)/4];
 }
-
-static inline void host_write(void *addr, int len, word_t data) {
-  switch (len) {
-    case 1: *(uint8_t  *)addr = data; return;
-    case 2: *(uint16_t *)addr = data; return;
-    case 4: *(uint32_t *)addr = data; return;
-    case 8: *(uint64_t *)addr = data; return;
-    default: assert(0);
-  }
-}
-
-
-uint8_t* guest_to_host(paddr_t paddr) { return pmem + paddr - CONFIG_MBASE; }
-paddr_t host_to_guest(uint8_t *haddr) { return haddr - pmem + CONFIG_MBASE; }
-
-static word_t pmem_read(paddr_t addr, int len) {
-  word_t ret = host_read(guest_to_host(addr), len);
-  return ret;
-}
-
-static void pmem_write(paddr_t addr, int len, word_t data) {
-  host_write(guest_to_host(addr), len, data);
-}
-
-static void out_of_bound(paddr_t addr) {
-  panic("address = " FMT_PADDR " is out of bound of pmem [" FMT_PADDR ", " FMT_PADDR ") at pc = " FMT_WORD,
-      addr, CONFIG_MBASE, CONFIG_MBASE + CONFIG_MSIZE, cpu.pc);
-}
-
-void init_mem() {
-#if   defined(CONFIG_PMEM_MALLOC)
-  pmem = malloc(CONFIG_MSIZE);
-  assert(pmem);
-#endif
-#ifdef CONFIG_MEM_RANDOM
-  uint32_t *p = (uint32_t *)pmem;
-  int i;
-  for (i = 0; i < (int) (CONFIG_MSIZE / sizeof(p[0])); i ++) {
-    p[i] = rand();
-  }
-#endif
-  Log("physical memory area [" FMT_PADDR ", " FMT_PADDR "]",
-      (paddr_t)CONFIG_MBASE, (paddr_t)CONFIG_MBASE + CONFIG_MSIZE);
-}
-
-word_t paddr_read(paddr_t addr, int len) {
-  if (likely(in_pmem(addr))) return pmem_read(addr, len);
-  IFDEF(CONFIG_DEVICE, return mmio_read(addr, len));
-#ifdef CONFIG_MTRACE
+void pmem_init(char *s){ 
+  //init inst
   FILE *fp;
-  fp=fopen("/home/agustin/ysyx-workbench/nemu/build/nemu-mtrace.txt","a");
-  assert(fp);
-  fprintf(fp,"pc=0x%08lx:\tpaddr_read address=%08x\tlen=%d\n",cpu.pc,addr,len);
-  fclose(fp); 
-#endif
-  out_of_bound(addr);
-  return 0;
+  fp=fopen(s,"r");
+  assert(fp!=NULL);
+  fseek(fp, 0, SEEK_END);
+  int size = ftell(fp);
+  size=size/4;
+  fseek(fp,0,SEEK_SET);
+  int ret=fread(pmem,4,size,fp);
+  assert(ret!=0);
+  fclose(fp);
+
+  //inst data
+  for(int i=0;i<pmem_size;i++){
+    pmem_data[i]=0;
+  }
+  /*for(int i=0;i<size;i++){
+    printf("%08x\n",pmem[i]);
+  }*/
+}
+u_int64_t pmem_data_read(u_int64_t addr){
+  return pmem_data[addr-0x80000000];
+}
+void pmem_data_write(u_int64_t addr,u_int64_t val){
+  pmem_data[addr-0x80000000]=val;
 }
 
-void paddr_write(paddr_t addr, int len, word_t data) {
-  if (likely(in_pmem(addr))) { pmem_write(addr, len, data); return; }
-  IFDEF(CONFIG_DEVICE, mmio_write(addr, len, data); return);
-#ifdef CONFIG_MTRACE
-  FILE *fp;
-  fp=fopen("/home/agustin/ysyx-workbench/nemu/build/nemu-mtrace.txt","a");
-  assert(fp);
-  fprintf(fp,"pc=0x%08lx:\tpaddr_write address=%08x\tlen=%d\n",cpu.pc,addr,len);
-  fclose(fp); 
-#endif 
-  out_of_bound(addr);
-}
 

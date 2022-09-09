6ba43afa3dc0e2bc34144e09aa2e211118e6b126
tracer-ysyx2204 <tracer@ysyx.org>
2022-09-10 00:35:41 +0800

    > compile NEMU ysyx_22040905 程雨航 Linux agustin-RedmiBook-Pro-14S 5.15.0-25-generic #25-Ubuntu SMP Wed Mar 30 15:54:22 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  00:35:41 up  4:56,  1 user,  load average: 1.67, 0.75, 0.55

--- abstract-machine/am/src/platform/nemu/ioe/timer.c
+++ abstract-machine/am/src/platform/nemu/ioe/timer.c
@@ -19,7 +19,7 @@ void __am_timer_init() {
 
 void __am_timer_uptime(AM_TIMER_UPTIME_T *uptime) {
   uptime->us = read_time() - boot_time;
-  printf("%d\n",uptime->us);
+  //printf("%d\n",uptime->us);
 }
 
 void __am_timer_rtc(AM_TIMER_RTC_T *rtc) {

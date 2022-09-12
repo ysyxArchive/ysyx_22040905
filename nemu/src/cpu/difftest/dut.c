751716abc4c04cb088168b744b3e127ce7a49777
tracer-ysyx2204 <tracer@ysyx.org>
2022-09-12 19:33:05 +0800

    > compile NEMU ysyx_22040905 程雨航 Linux agustin-RedmiBook-Pro-14S 5.15.0-25-generic #25-Ubuntu SMP Wed Mar 30 15:54:22 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  19:33:05 up  3:52,  1 user,  load average: 0.43, 0.91, 1.14

--- nemu/src/cpu/difftest/dut.c
+++ nemu/src/cpu/difftest/dut.c
@@ -116,7 +116,7 @@ void difftest_step(vaddr_t pc, vaddr_t npc) {
 void init_difftest(char *ref_so_file, long img_size, int port) { }
 #endif
 
-#ifndef CONFIG_ITRACE
+#ifdef CONFIG_ITRACE
 #ifndef ir_max
 #define ir_max 10
 #endif

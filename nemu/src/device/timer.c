130ac4740a2d281732106dc879d5eab6e7c3c9ec
tracer-ysyx2204 <tracer@ysyx.org>
2022-09-12 18:19:17 +0800

    > compile NEMU ysyx_22040905 程雨航 Linux agustin-RedmiBook-Pro-14S 5.15.0-25-generic #25-Ubuntu SMP Wed Mar 30 15:54:22 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  18:19:17 up  2:39,  1 user,  load average: 0.34, 0.35, 0.37

--- nemu/src/device/timer.c
+++ nemu/src/device/timer.c
@@ -5,11 +5,16 @@
 static uint32_t *rtc_port_base = NULL;
 
 static void rtc_io_handler(uint32_t offset, int len, bool is_write) {
-  assert(offset == 0 || offset == 4);
-  if (!is_write && offset == 4) {
+  assert(offset == 0 || offset == 0);
+  if (!is_write && offset == 0) {
     uint64_t us = get_time();
     rtc_port_base[0] = (uint32_t)us;
     rtc_port_base[1] = us >> 32;
+  }
+  if (!is_write && offset == 4) {
+    uint64_t us = get_time();
+    rtc_port_base[0] = (uint32_t)us;
+    rtc_port_base[1] = 0;
   }
 }
 

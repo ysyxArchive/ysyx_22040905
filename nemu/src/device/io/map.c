3a553516175bf125e68f5909853da46d6ef50efe
tracer-ysyx2204 <tracer@ysyx.org>
2022-09-16 19:43:35 +0800

    > compile NEMU ysyx_22040905 程雨航 Linux agustin-RedmiBook-Pro-14S 5.15.0-25-generic #25-Ubuntu SMP Wed Mar 30 15:54:22 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  19:43:35 up 43 min,  1 user,  load average: 0.67, 0.90, 0.82

--- nemu/src/device/io/map.c
+++ nemu/src/device/io/map.c
@@ -45,7 +45,7 @@ word_t map_read(paddr_t addr, int len, IOMap *map) {
   word_t ret = host_read(map->space + offset, len);
 #ifdef CONFIG_DTRACE
   FILE *fp=fopen("/home/agustin/ysyx-workbench/nemu/build/nemu-dtrace.txt", "a");
-  fprintf(fp,"pc:%08lx\tread %s\tlen:%d\tdata: %ld",cpu.pc,map->name,len,ret);
+  fprintf(fp,"pc:%08lx\tread %s\tlen:%d\tdata: %ld\n",cpu.pc,map->name,len,ret);
   fclose(fp);
 #endif
   return ret;
@@ -60,7 +60,7 @@ void map_write(paddr_t addr, int len, word_t data, IOMap *map) {
   invoke_callback(map->callback, offset, len, true);
 #ifdef CONFIG_DTRACE
   FILE *fp=fopen("/home/agustin/ysyx-workbench/nemu/build/nemu-dtrace.txt", "a");
-  fprintf(fp,"pc:%08lx\twrite %s\tlen:%d\tdata: %ld",cpu.pc,map->name,len,data);
+  fprintf(fp,"pc:%08lx\twrite %s\tlen:%d\tdata: %ld\n",cpu.pc,map->name,len,data);
   fclose(fp);
 #endif
 

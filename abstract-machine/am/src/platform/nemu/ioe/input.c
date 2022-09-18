a361dd4bbefcfcaad68125d1a993d05560601a42
tracer-ysyx2204 <tracer@ysyx.org>
2022-09-18 13:11:16 +0800

    > compile NEMU ysyx_22040905 程雨航 Linux agustin-RedmiBook-Pro-14S 5.15.0-25-generic #25-Ubuntu SMP Wed Mar 30 15:54:22 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  13:11:16 up 46 min,  1 user,  load average: 0.74, 1.52, 1.15

--- abstract-machine/am/src/platform/nemu/ioe/input.c
+++ abstract-machine/am/src/platform/nemu/ioe/input.c
@@ -7,7 +7,8 @@
 void __am_input_keybrd(AM_INPUT_KEYBRD_T *kbd) {
   kbd->keydown = 0; //inb(KBD_ADDR);
   kbd->keycode = AM_KEY_NONE;
-  for(int i=0;i<4;i+=4){
-    printf("%d\n",inb(KBD_ADDR+i));
+  for(int i=0;i<4;i++){
+    printf("%lx ",inb(KBD_ADDR+i));
   }
+  printf("\n");
 }

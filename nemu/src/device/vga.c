55a8f7bf0f558591fa3aff69a9c5a09f49b121ce
tracer-ysyx2204 <tracer@ysyx.org>
2022-09-18 19:50:05 +0800

    > compile NEMU ysyx_22040905 程雨航 Linux agustin-RedmiBook-Pro-14S 5.15.0-25-generic #25-Ubuntu SMP Wed Mar 30 15:54:22 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  19:50:05 up  7:24,  1 user,  load average: 0.67, 0.39, 0.30

--- nemu/src/device/vga.c
+++ nemu/src/device/vga.c
@@ -54,8 +54,8 @@ static inline void update_screen() {
 }
 #endif
 #endif
-
 void vga_update_screen() {
+  //  update_screen();
   // TODO: call `update_screen()` when the sync register is non-zero,
   // then zero out the sync register
 }

23826cd72114652c4b245a3cc0915503fa23bdfb
tracer-ysyx2204 <tracer@ysyx.org>
2022-09-26 13:28:38 +0800

    > compile NEMU ysyx_22040905 程雨航 Linux agustin-RedmiBook-Pro-14S 5.15.0-25-generic #25-Ubuntu SMP Wed Mar 30 15:54:22 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  13:28:38 up 49 min,  1 user,  load average: 0.33, 0.44, 0.57

--- abstract-machine/am/src/platform/nemu/ioe/gpu.c
+++ abstract-machine/am/src/platform/nemu/ioe/gpu.c
@@ -39,9 +39,11 @@ uint32_t *fb = (uint32_t *)(uintptr_t)FB_ADDR;
 void __am_gpu_fbdraw(AM_GPU_FBDRAW_T *ctl) {
   //for(int i=0;i<ww*hh;i++) fb[i]=0;
   int x = ctl->x, y = ctl->y, w = ctl->w, h = ctl->h;
+  int cnt=0;
   for(int i=0;i<h;i++)
   for(int j=0;j<w;j++){
-    fb[(y+i)*W+x+j]=(*((uint32_t *)(ctl->pixels)));
+    fb[(y+i)*W+x+j]=(*((uint32_t *)(ctl->pixels)+cnt));
+    cnt++;
   }
 
   if (ctl->sync) {

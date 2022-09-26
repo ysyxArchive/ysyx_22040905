913089f24e6c42c6846c0089cfc8019266b0c2d6
tracer-ysyx2204 <tracer@ysyx.org>
2022-09-26 13:17:48 +0800

    > compile NEMU ysyx_22040905 程雨航 Linux agustin-RedmiBook-Pro-14S 5.15.0-25-generic #25-Ubuntu SMP Wed Mar 30 15:54:22 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  13:17:48 up 38 min,  1 user,  load average: 1.86, 1.21, 0.81

--- nemu/src/device/timer.c
+++ nemu/src/device/timer.c
@@ -11,11 +11,11 @@ static void rtc_io_handler(uint32_t offset, int len, bool is_write) {
     rtc_port_base[0] = (uint32_t)us;
     rtc_port_base[1] = us >> 32;
   }
-//  if (!is_write && offset == 4) {
-//    uint64_t us = get_time();
-//    rtc_port_base[0] = (uint32_t)us;
-//    rtc_port_base[1] = 0;
-//  }
+  if (!is_write && offset == 4) {
+    uint64_t us = get_time();
+    rtc_port_base[0] = (uint32_t)us;
+    rtc_port_base[1] = 0;
+  }
 }
 
 #ifndef CONFIG_TARGET_AM

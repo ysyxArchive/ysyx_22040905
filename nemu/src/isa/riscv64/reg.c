42c092adaef1b4b8c46eb61d6b38233596069a64
tracer-ysyx2204 <tracer@ysyx.org>
2022-10-06 00:21:25 +0800

    > compile NEMU ysyx_22040905 程雨航 Linux agustin-RedmiBook-Pro-14S 5.15.0-25-generic #25-Ubuntu SMP Wed Mar 30 15:54:22 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  00:21:25 up  1:45,  1 user,  load average: 2.19, 1.04, 0.93

--- nemu/src/isa/riscv64/reg.c
+++ nemu/src/isa/riscv64/reg.c
@@ -11,13 +11,24 @@ const char *regs[] = {
 void isa_reg_display() {
   printf("%s\t\t%ld\n","pc",cpu.pc);
   for(int i=0;i<32;i++){
-    printf("%s\t\t%ld\n",reg_name(i,8),gpr(i));
+    printf("%s\t%ld\t\t",reg_name(i,8),gpr(i));
+    if((i+1)%4==0)printf("\n");
   }
+  printf("cpu.mcause\t%ld\t\tcpu.mstatus\t%ld\t\tcpu.mepc\t%ld\n",cpu.mcause,cpu.mstatus, cpu.mepc);
 }
 
 word_t isa_reg_str2val(const char *s, bool *success) {
   if(strcmp(s,"$pc")==0){
     return cpu.pc;
+  }  
+  if(strcmp(s,"$mcause")==0){
+    return cpu.mcause;
+  }  
+  if(strcmp(s,"$mstatus")==0){
+    return cpu.mstatus;
+  }  
+  if(strcmp(s,"$mepc")==0){
+    return cpu.mepc;
   }
   for(int i=0;i<32;i++){
     if(strcmp(regs[i],s+1)==0||((!i)&&(strcmp(s,"$0")))){//ignore '$',except "$0"

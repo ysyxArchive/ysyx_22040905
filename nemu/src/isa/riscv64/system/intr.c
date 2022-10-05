47af2e079e0cbef688f9aa52fd54b16613a8c8d6
tracer-ysyx2204 <tracer@ysyx.org>
2022-10-06 01:33:22 +0800

    > compile NEMU ysyx_22040905 程雨航 Linux agustin-RedmiBook-Pro-14S 5.15.0-25-generic #25-Ubuntu SMP Wed Mar 30 15:54:22 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  01:33:22 up  2:57,  1 user,  load average: 0.71, 0.87, 0.82

--- nemu/src/isa/riscv64/system/intr.c
+++ nemu/src/isa/riscv64/system/intr.c
@@ -13,7 +13,7 @@ word_t isa_raise_intr(word_t NO, vaddr_t epc) {
 	FILE *fp;
   fp=fopen("/home/agustin/ysyx-workbench/nemu/build/nemu-etrace.txt","a");
   assert(fp);
-  fprintf(fp,"pc:%lx\tmcause:%lx\tcpu.mtvec:%lx\n",cpu.mepc,cpu.mcause,cpu.mtvec);
+  fprintf(fp,"pc:0x%lx\tmcause:0x%lx\tcpu.mtvec:0x%lx\n",cpu.mepc,cpu.mcause,cpu.mtvec);
   fclose(fp); 
 #endif
     return cpu.mtvec;

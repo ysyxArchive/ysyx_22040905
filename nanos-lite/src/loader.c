6425aeb97ca4e5a73ca26fee08b3e10e5d6edd24
tracer-ysyx2204 <tracer@ysyx.org>
2022-10-08 22:54:39 +0800

    > compile NEMU ysyx_22040905 程雨航 Linux agustin-RedmiBook-Pro-14S 5.15.0-25-generic #25-Ubuntu SMP Wed Mar 30 15:54:22 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  22:54:39 up  3:08,  1 user,  load average: 0.62, 0.69, 0.55

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -36,9 +36,8 @@ static uintptr_t loader(PCB *pcb, const char *filename) {
 }
 
 void naive_uload(PCB *pcb, const char *filename) {
-  //uintptr_t entry = 
-  loader(pcb, filename);
-  //Log("Jump to entry = %p", (void *)entry);
-  //((void(*)())entry) ();
+  uintptr_t entry = loader(pcb, filename);
+  Log("Jump to entry = %p", (void *)entry);
+  ((void(*)())entry) ();
 }
 

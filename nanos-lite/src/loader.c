e43bccb736933ed156022e4ebb4d6033f6f2f84a
tracer-ysyx2204 <tracer@ysyx.org>
2022-10-08 21:23:31 +0800

    > compile NEMU ysyx_22040905 程雨航 Linux agustin-RedmiBook-Pro-14S 5.15.0-25-generic #25-Ubuntu SMP Wed Mar 30 15:54:22 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  21:23:31 up  1:36,  1 user,  load average: 0.41, 0.60, 0.57

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -1,6 +1,6 @@
 #include <proc.h>
 #include <elf.h>
-
+#include <stdio.h>
 #ifdef __LP64__
 # define Elf_Ehdr Elf64_Ehdr
 # define Elf_Phdr Elf64_Phdr
@@ -9,14 +9,30 @@
 # define Elf_Phdr Elf32_Phdr
 #endif
 
+extern uint8_t ramdisk_start;
+size_t ramdisk_read(void *buf, size_t offset, size_t len);
+size_t ramdisk_write(const void *buf, size_t offset, size_t len);
+size_t get_ramdisk_size();
+
 static uintptr_t loader(PCB *pcb, const char *filename) {
-  TODO();
-  return 0;
+  /*Elf_Ehdr ehdr[1];
+  FILE *fp = &ramdisk_start;
+  fseek(fp,0,SEEK_SET);
+  int ret=fread(ehdr, sizeof(Elf64_Ehdr), 1, fp);
+  assert(ret!=0);
+  printf("%lx\n",ehdr->e_ident);
+  //assert(*(uint64_t *)ehdr->e_ident == 0xBadC0de);*/
+  char buf[40];
+  int len=ramdisk_read(buf,0,4);
+  for(int i=0;i<len;i++){
+  printf("%c\n",*(buf+i));
+  }
+  return (uint64_t)&ramdisk_start;
 }
 
 void naive_uload(PCB *pcb, const char *filename) {
   uintptr_t entry = loader(pcb, filename);
-  Log("Jump to entry = %p", entry);
+  Log("Jump to entry = %p", (void *)entry);
   ((void(*)())entry) ();
 }
 

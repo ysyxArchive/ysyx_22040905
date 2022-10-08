d821b2f9a03210dd1743bcce8e916d804d877485
tracer-ysyx2204 <tracer@ysyx.org>
2022-10-08 23:27:49 +0800

    > compile NEMU ysyx_22040905 程雨航 Linux agustin-RedmiBook-Pro-14S 5.15.0-25-generic #25-Ubuntu SMP Wed Mar 30 15:54:22 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  23:27:49 up  3:41,  1 user,  load average: 0.89, 0.56, 0.64

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -20,7 +20,7 @@ static uintptr_t loader(PCB *pcb, const char *filename) {
   Elf_Ehdr ehdr[1];
   Elf_Phdr phdr[2048];
   ramdisk_read(ehdr,0,get_ramdisk_size());
-  //printf("%x\n",*(uint32_t *)ehdr->e_ident);
+  printf("%x\n",*(uint32_t *)ehdr->e_ident);
   assert(*(uint32_t *)ehdr->e_ident == 0x464c457f);
   int num=ehdr->e_phnum;
   memcpy(phdr,ehdr+(ehdr->e_phoff),ehdr->e_phentsize);

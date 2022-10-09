b2be915162d7934899f58ff5ef4093033a8a79bd
tracer-ysyx2204 <tracer@ysyx.org>
2022-10-09 16:46:46 +0800

    > compile NEMU ysyx_22040905 程雨航 Linux agustin-RedmiBook-Pro-14S 5.15.0-25-generic #25-Ubuntu SMP Wed Mar 30 15:54:22 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  16:46:46 up 21 min,  1 user,  load average: 1.30, 1.04, 0.64

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -27,8 +27,10 @@ static uintptr_t loader(PCB *pcb, const char *filename) {
     if(phdr[i].p_type==PT_LOAD){
       memcpy(buf,ehdr+ehdr->e_phoff+(phdr[i].p_offset),phdr[i].p_filesz);
       memset(buf+phdr[i].p_filesz,0,phdr[i].p_memsz-phdr[i].p_filesz);
-      memcpy((void *)(phdr[i].p_vaddr),buf,phdr[i].p_memsz); 
+      memcpy((void *)(phdr[i].p_vaddr),buf,phdr[i].p_memsz);
+      for(int i=0;i<phdr[i].p_memsz;i++){ 
       printf("%x\n",buf[0]);
+      }
       printf("************\n");
       //printf("%lx\t%lx\t%lx\t%lx\n",phdr[i].p_offset,vhdr[i].p_paddr,phdr[i].p_filesz,phdr[i].p_memsz);
     }

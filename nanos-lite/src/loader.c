2540cfadfe46610ac9d3b773cfc9fa5a267e7099
tracer-ysyx2204 <tracer@ysyx.org>
2022-10-09 22:18:04 +0800

    > compile NEMU ysyx_22040905 程雨航 Linux agustin-RedmiBook-Pro-14S 5.15.0-25-generic #25-Ubuntu SMP Wed Mar 30 15:54:22 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  22:18:04 up  5:53,  1 user,  load average: 0.32, 0.45, 0.50

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -15,31 +15,21 @@ size_t ramdisk_write(const void *buf, size_t offset, size_t len);
 size_t get_ramdisk_size();
 
 
-uint8_t buf[131072];
+char buf[1048576];
 static uintptr_t loader(PCB *pcb, const char *filename) {
-  Elf_Ehdr ehdr[1];
-  Elf_Phdr phdr[2048];
-  ramdisk_read(ehdr,0,get_ramdisk_size());
+  ramdisk_read(buf,0,get_ramdisk_size());
+  Elf_Ehdr* ehdr=(Elf_Ehdr*)buf;
+  Elf_Phdr* phdr=(Elf_Phdr*)(buf+ehdr->e_phoff);
   assert(*(uint32_t *)ehdr->e_ident == 0x464c457f);
-  int num=ehdr->e_phnum;
-  int size=ehdr->e_phentsize;
-  for(int i=0;i<num;i++){
-  memcpy(phdr+i*size,ehdr+(ehdr->e_phoff)+i*size,size);
-  }
-  for(int i=0;i<num;i++){
-    if(phdr[i].p_type==PT_LOAD){
-      memcpy(buf,ehdr+ehdr->e_phoff+(phdr[i].p_offset),phdr[i].p_filesz);
-      memset(buf+phdr[i].p_filesz,0,phdr[i].p_memsz-phdr[i].p_filesz);
-      memcpy((void *)(phdr[i].p_vaddr),buf,phdr[i].p_memsz);
-      printf("%lx %lx\n",ehdr->e_phoff+(phdr[i].p_offset),phdr[i].p_filesz);
-      for(uint64_t j=0;j<phdr[i].p_memsz;j++){ 
-      //printf("%02x",buf[j]);
-      //if((j+1)%16==0)printf("\n");
+  for(unsigned i=0;i<ehdr->e_phnum;i++){
+    if(phdr[i].p_type==PT_LOAD &&phdr[i].p_memsz){
+      if(phdr[i].p_filesz){
+        memcpy((void *)phdr[i].p_vaddr,(uint8_t*)buf+phdr[i].p_offset,phdr[i].p_filesz);
       }
-      printf("************\n");
-      //printf("%lx\t%lx\t%lx\t%lx\n",phdr[i].p_offset,vhdr[i].p_paddr,phdr[i].p_filesz,phdr[i].p_memsz);
+      memset((void *)phdr[i].p_vaddr+phdr[i].p_filesz,0,phdr[i].p_memsz-phdr[i].p_filesz);
     }
   }
+
   return (uintptr_t)(ehdr->e_entry);
 }
 

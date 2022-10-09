9a26106e4426dd0c6aaf37b073ba5963ccfb6168
tracer-ysyx2204 <tracer@ysyx.org>
2022-10-09 17:36:39 +0800

    > compile NEMU ysyx_22040905 程雨航 Linux agustin-RedmiBook-Pro-14S 5.15.0-25-generic #25-Ubuntu SMP Wed Mar 30 15:54:22 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  17:36:39 up  1:11,  1 user,  load average: 0.23, 0.63, 0.65

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -22,10 +22,10 @@ static uintptr_t loader(PCB *pcb, const char *filename) {
   ramdisk_read(ehdr,0,get_ramdisk_size());
   assert(*(uint32_t *)ehdr->e_ident == 0x464c457f);
   int num=ehdr->e_phnum;
-  memcpy(phdr,ehdr+(ehdr->e_phoff),ehdr->e_phentsize);
+  memcpy(phdr,ehdr+(ehdr->e_phoff),(ehdr->e_phentsize)*(ehdr->e_phnum));
   for(int i=0;i<num;i++){
     if(phdr[i].p_type==PT_LOAD){
-      memcpy(buf,(uintptr_t *)(ehdr->e_entry+(phdr[i].p_offset)),phdr[i].p_filesz);
+      memcpy(buf,ehdr+ehdr->e_phoff+(phdr[i].p_offset),phdr[i].p_filesz);
       memset(buf+phdr[i].p_filesz,0,phdr[i].p_memsz-phdr[i].p_filesz);
       memcpy((void *)(phdr[i].p_vaddr),buf,phdr[i].p_memsz);
       printf("%lx %lx\n",ehdr->e_phoff+(phdr[i].p_offset),phdr[i].p_filesz);

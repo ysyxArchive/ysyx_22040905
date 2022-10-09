68f8bf1c3090c9f32a2a3b5a66282fbd7839a419
tracer-ysyx2204 <tracer@ysyx.org>
2022-10-10 00:37:29 +0800

    > compile NEMU ysyx_22040905 程雨航 Linux agustin-RedmiBook-Pro-14S 5.15.0-25-generic #25-Ubuntu SMP Wed Mar 30 15:54:22 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  00:37:29 up  8:12,  1 user,  load average: 0.40, 0.61, 0.72

--- nemu/src/isa/riscv64/system/intr.c
+++ nemu/src/isa/riscv64/system/intr.c
@@ -7,7 +7,7 @@ word_t isa_raise_intr(word_t NO, vaddr_t epc) {
    */
   if(NO==1){
     cpu.mepc=epc;
-    if(epc>0x83000000)cpu.mcause=0xb;
+    if(epc<0x83000000)cpu.mcause=0xb;
     else cpu.mcause=0x3;
 #ifdef CONFIG_ETRACE	  
 	FILE *fp;

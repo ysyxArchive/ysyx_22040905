a3d4a6660156b901a800ebb4d04a0ad9acdee89d
tracer-ysyx2204 <tracer@ysyx.org>
2022-10-16 10:57:05 +0800

    > compile NEMU ysyx_22040905 程雨航 Linux agustin-RedmiBook-Pro-14S 5.15.0-25-generic #25-Ubuntu SMP Wed Mar 30 15:54:22 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  10:57:05 up 20 min,  1 user,  load average: 0.50, 0.40, 0.37

--- nemu/src/isa/riscv64/system/intr.c
+++ nemu/src/isa/riscv64/system/intr.c
@@ -5,9 +5,7 @@ word_t isa_raise_intr(word_t NO, vaddr_t epc) {
   /* TODO: Trigger an interrupt/exception with ``NO''.
    * Then return the address of the interrupt/exception vector.
    */
-  if(NO==1){
-    cpu.mepc=epc;
-    cpu.mcause=0xb;
+  if(!NO){
 #ifdef CONFIG_ETRACE	  
 	FILE *fp;
   fp=fopen("/home/agustin/ysyx-workbench/nemu/build/nemu-etrace.txt","a");
@@ -15,9 +13,14 @@ word_t isa_raise_intr(word_t NO, vaddr_t epc) {
   fprintf(fp,"pc:0x%lx\tmcause:0x%lx\tcpu.mtvec:0x%lx\n",cpu.mepc,cpu.mcause,cpu.mtvec);
   fclose(fp); 
 #endif
-    return cpu.mtvec;
   }
-  else if(NO==2){
+  if(NO==1){ 
+    cpu.mepc=epc;
+    cpu.mcause=0xb;
+   return cpu.mtvec;
+  }
+  else if(NO==0){
+    printf("%lx\n",cpu.mepc);
     return cpu.mepc;
   }
   return 0;

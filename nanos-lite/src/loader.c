323de8b83bfcbe41631f60ad6465f9f003b1b6b9
tracer-ysyx2204 <tracer@ysyx.org>
2022-10-19 22:22:01 +0800

    > compile NEMU ysyx_22040905 程雨航 Linux agustin-RedmiBook-Pro-14S 5.15.0-25-generic #25-Ubuntu SMP Wed Mar 30 15:54:22 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  22:22:01 up 58 min,  1 user,  load average: 0.75, 0.54, 0.46

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -30,7 +30,7 @@ static uintptr_t loader(PCB *pcb, const char *filename) {
       memset((uint8_t *)phdr[i].p_vaddr+phdr[i].p_filesz,0,phdr[i].p_memsz-phdr[i].p_filesz);
     }
   }
-  return (uintptr_t)(ehdr->e_entry);
+  return (uintptr_t)(0x83000000);//ehdr->e_entry);
 }
 
 void naive_uload(PCB *pcb, const char *filename) {

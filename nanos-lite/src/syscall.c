55fa9c6ef353e4f2960a4cd9b962d41045e57a0b
tracer-ysyx2204 <tracer@ysyx.org>
2022-10-20 00:08:54 +0800

    > compile NEMU ysyx_22040905 程雨航 Linux agustin-RedmiBook-Pro-14S 5.15.0-25-generic #25-Ubuntu SMP Wed Mar 30 15:54:22 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  00:08:54 up  2:45,  1 user,  load average: 0.47, 0.41, 0.43

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -13,7 +13,6 @@ void SYS_exit(uintptr_t code){
 void do_syscall(Context *c) {
   uintptr_t a[4];
   a[0] = c->GPR1;
-  printf("%lx\n",a[0]);
   switch (a[0]) {
     case 0: SYS_exit(c->GPRx);break;
     case 1: c->GPRx=SYS_yield();break; 

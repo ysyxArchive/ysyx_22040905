27b3852f43a103ce581f96edb794f2ca373e5242
tracer-ysyx2204 <tracer@ysyx.org>
2022-10-19 23:11:55 +0800

    > compile NEMU ysyx_22040905 程雨航 Linux agustin-RedmiBook-Pro-14S 5.15.0-25-generic #25-Ubuntu SMP Wed Mar 30 15:54:22 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  23:11:55 up  1:48,  1 user,  load average: 1.24, 0.67, 0.52

--- nanos-lite/src/irq.c
+++ nanos-lite/src/irq.c
@@ -3,7 +3,7 @@
 static Context* do_event(Event e, Context* c) {
   switch (e.event) {
     case 1: printf("YIELD\n");break;
-    case 2: do_syscall(c); 
+    case 2: do_syscall(c);break; 
     default: panic("Unhandled event ID = %d", e.event);
   }
 

152288d34fa6e4d4a6b5462340ab6322b500437b
tracer-ysyx2204 <tracer@ysyx.org>
2022-10-20 14:46:38 +0800

    > compile NEMU ysyx_22040905 程雨航 Linux agustin-RedmiBook-Pro-14S 5.15.0-25-generic #25-Ubuntu SMP Wed Mar 30 15:54:22 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  14:46:38 up  2:31,  1 user,  load average: 1.12, 0.92, 0.85

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -11,13 +11,14 @@ size_t SYS_exit(uintptr_t code){
   return 0;
 }
 
-size_t SYS_write(int fd,uintptr_t *buf,size_t count){
+size_t SYS_write(int fd,const void *buf,size_t count){
+  char *buff=(char *)buf;
   if(!((fd==1)||(fd==2))) return -1;//error
   int i=0;
-  for(;i<count&&((*(buf+i))!='\0');i++){
-  putch(*(buf+i));
+  for(;i<count&&((*(buff+i))!='\0');i++){
+  putch(*(buff+i));
   } 
-  printf("\n%c\n",*(buf+8));
+  printf("\n%c\n",*(buff+8));
   return i;
 }
 
@@ -35,7 +36,7 @@ void do_syscall(Context *c) {
   switch (a[0]) {
     case 0: SYS_exit(c->GPRx);break;
     case 1: c->GPRx=SYS_yield();break;
-    case 4: c->GPRx=SYS_write(c->GPR2,(uintptr_t *)c->GPR3,c->GPR4);break; 
+    case 4: c->GPRx=SYS_write(c->GPR2,(void *)c->GPR3,c->GPR4);break; 
     default: panic("Unhandled syscall ID = %d", a[0]);
   }
 }

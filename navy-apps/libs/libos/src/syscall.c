9cb16446e8fea8120f2eafe26cb47431cc349bf2
tracer-ysyx2204 <tracer@ysyx.org>
2022-10-20 14:23:56 +0800

    > compile NEMU ysyx_22040905 程雨航 Linux agustin-RedmiBook-Pro-14S 5.15.0-25-generic #25-Ubuntu SMP Wed Mar 30 15:54:22 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  14:23:56 up  2:08,  1 user,  load average: 1.98, 1.01, 0.68

--- navy-apps/libs/libos/src/syscall.c
+++ navy-apps/libs/libos/src/syscall.c
@@ -51,7 +51,6 @@ intptr_t _syscall_(intptr_t type, intptr_t a0, intptr_t a1, intptr_t a2) {
 }
 
 void _exit(int status) {
-  printf("1111\n");
   _syscall_(SYS_exit, status, 0, 0);
   while (1);
 }
@@ -62,7 +61,7 @@ int _open(const char *path, int flags, mode_t mode) {
 }
 
 int _write(int fd, void *buf, size_t count) {
-  _syscall_(SYS_write, fd,(intptr_t *)buf, count);
+  _syscall_(SYS_write, fd,(intptr_t )buf, count);
   return 0;
 }
 

e51aeb3afbba170322962f0c63873253f933ebe9
tracer-ysyx2204 <tracer@ysyx.org>
2022-10-20 14:30:03 +0800

    > compile NEMU ysyx_22040905 程雨航 Linux agustin-RedmiBook-Pro-14S 5.15.0-25-generic #25-Ubuntu SMP Wed Mar 30 15:54:22 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  14:30:03 up  2:15,  1 user,  load average: 0.92, 1.34, 1.00

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -13,8 +13,8 @@ size_t SYS_exit(uintptr_t code){
 
 size_t SYS_write(int fd,uintptr_t *buf,size_t count){
   printf("%d\n",fd);
-  if((fd!=1)||(fd!=2)) return -1;//error
-  printf("1\n");
+  if(!((fd==1)||(fd==2))) return -1;//error
+  printf("2\n");
   int i=0;
   for(;i<count&&((*(buf+i))!='\0');i++){
   putch(*(buf+i));

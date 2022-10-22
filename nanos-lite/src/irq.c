a59dc46025556e43549cda90f9e623070a11e815
tracer-ysyx2204 <tracer@ysyx.org>
2022-10-22 23:03:08 +0800

    > compile NEMU ysyx_22040905 程雨航 Linux agustin-RedmiBook-Pro-14S 5.15.0-25-generic #25-Ubuntu SMP Wed Mar 30 15:54:22 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  23:03:08 up  3:12,  1 user,  load average: 0.79, 0.51, 0.50

--- nanos-lite/src/irq.c
+++ nanos-lite/src/irq.c
@@ -1,5 +1,7 @@
 #include <common.h>
 #include "syscall.h"
+extern void do_syscall(Context *c);
+
 static Context* do_event(Event e, Context* c) {
   switch (e.event) {
     case 1: printf("YIELD\n");break;
--- navy-apps/libs/libos/src/syscall.h
+++ navy-apps/libs/libos/src/syscall.h
@@ -23,5 +23,4 @@ enum {
   SYS_times,
   SYS_gettimeofday
 };
-void do_syscall(Context *c);
 #endif

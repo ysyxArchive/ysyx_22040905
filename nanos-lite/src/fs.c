fab0591f29d9989e412b63f0f4f969dbc4c36f36
tracer-ysyx2204 <tracer@ysyx.org>
2022-10-22 23:20:02 +0800

    > compile NEMU ysyx_22040905 程雨航 Linux agustin-RedmiBook-Pro-14S 5.15.0-25-generic #25-Ubuntu SMP Wed Mar 30 15:54:22 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  23:20:02 up  3:29,  1 user,  load average: 0.41, 0.42, 0.50

--- nanos-lite/src/fs.c
+++ nanos-lite/src/fs.c
@@ -47,7 +47,7 @@ int fs_open(const char *pathname, int flags, int mode){
 }
 size_t fs_read(int fd, void *buf, size_t len){
   assert(file_table[fd].open_offset+len<=file_table[fd].size);
-  ramdisk_read(buf,file_table[fd].disk_offset,len);
+  ramdisk_read(buf,file_table[fd].disk_offset+file_table[fd].open_offset,len);
   file_table[fd].open_offset+=len;
   return len;
 }
@@ -62,7 +62,7 @@ size_t fs_write(int fd, const void *buf, size_t len){
   }
   else{
     assert(file_table[fd].open_offset+len<=file_table[fd].size);
-    ramdisk_write(buf,file_table[fd].disk_offset,len);
+    ramdisk_write(buf,file_table[fd].disk_offset+file_table[fd].open_offset,len);
     file_table[fd].open_offset+=len;
   }
   return len;

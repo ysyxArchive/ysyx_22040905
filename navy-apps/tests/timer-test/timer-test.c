6f7de71a44e7134c44aeb2988f80158b81e6f869
tracer-ysyx2204 <tracer@ysyx.org>
2022-10-25 21:38:48 +0800

    > compile NEMU ysyx_22040905 程雨航 Linux agustin-RedmiBook-Pro-14S 5.15.0-25-generic #25-Ubuntu SMP Wed Mar 30 15:54:22 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  21:38:48 up  6:11,  1 user,  load average: 0.66, 0.68, 0.62

--- navy-apps/tests/timer-test/timer-test.c
+++ navy-apps/tests/timer-test/timer-test.c
@@ -6,7 +6,7 @@ int main() {
   uint32_t ms=NDL_GetTicks();
   int t=0; 
   while (1) {
-    if (NDL_GetTicks()-ms>=1000000) {
+    if (NDL_GetTicks()-ms>=500) {
       t+=1;
       printf("Hello World at the second %d.%d\n",t/2,(t%2)?5:0);
       ms=NDL_GetTicks() ;

55d33beee456969db398e4d755e7c047eb9ca4ea
tracer-ysyx2204 <tracer@ysyx.org>
2022-10-25 16:34:41 +0800

    > compile NEMU ysyx_22040905 程雨航 Linux agustin-RedmiBook-Pro-14S 5.15.0-25-generic #25-Ubuntu SMP Wed Mar 30 15:54:22 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  16:34:41 up  1:07,  1 user,  load average: 1.47, 0.87, 0.48

--- nanos-lite/src/fs.c
+++ nanos-lite/src/fs.c
@@ -54,7 +54,7 @@ size_t fs_read(int fd, void *buf, size_t len){
   return len;
 }
 size_t fs_write(int fd, const void *buf, size_t len){
-  if((fd==1)||(fd==2)){
+  if((file_table[fd].write!=NULL)){
   return file_table[fd].write(buf,0,len);
   }
   else{

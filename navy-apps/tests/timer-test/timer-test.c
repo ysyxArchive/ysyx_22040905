c7bd490d77f9815b4bea69f834db21506fe88118
tracer-ysyx2204 <tracer@ysyx.org>
2022-10-25 21:33:09 +0800

    > compile NEMU ysyx_22040905 程雨航 Linux agustin-RedmiBook-Pro-14S 5.15.0-25-generic #25-Ubuntu SMP Wed Mar 30 15:54:22 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  21:33:09 up  6:05,  1 user,  load average: 1.23, 0.67, 0.57

--- navy-apps/tests/timer-test/timer-test.c
+++ navy-apps/tests/timer-test/timer-test.c
@@ -3,11 +3,11 @@
 
 int main() {
   NDL_Init(0);
-  uint32_t us=NDL_GetTicks(); 
+  uint32_t ms=NDL_GetTicks(); 
   while (1) {
-    if (NDL_GetTicks()-us>=500000) {
+    if (NDL_GetTicks()-ms>=1000000) {
       printf("Hello World!\n");
-      us=NDL_GetTicks() ;
+      ms=NDL_GetTicks() ;
     }
   }
   return 0;

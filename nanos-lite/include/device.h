f98c2e8ca93f8fc46edc9196aa0f54ebcd9bd1d0
tracer-ysyx2204 <tracer@ysyx.org>
2022-10-25 16:22:29 +0800

    > compile NEMU ysyx_22040905 程雨航 Linux agustin-RedmiBook-Pro-14S 5.15.0-25-generic #25-Ubuntu SMP Wed Mar 30 15:54:22 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  16:22:29 up 55 min,  1 user,  load average: 0.24, 0.29, 0.23

---
+++ nanos-lite/include/device.h
@@ -0,0 +1,5 @@
+#ifndef __DEVICE_H__
+#define __DEVICE_H__
+
+size_t serial_write(const void *buf, size_t offset, size_t len);
+#endif
--- nanos-lite/src/device.c
+++ nanos-lite/src/device.c
@@ -15,7 +15,12 @@ static const char *keyname[256] __attribute__((used)) = {
 };
 
 size_t serial_write(const void *buf, size_t offset, size_t len) {
-  return 0;
+  int i=0;
+  char *buff=(char *)buf;
+  for(;i<len&&(*(buff+i)!='\0');i++){
+    putch(*(buff+i));
+  }
+  return i;
 }
 
 size_t events_read(void *buf, size_t offset, size_t len) {

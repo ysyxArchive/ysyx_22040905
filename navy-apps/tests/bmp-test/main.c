d0d4008a67eea60bf9bc7a304648cbbae2412f2e
tracer-ysyx2204 <tracer@ysyx.org>
2022-10-26 23:36:03 +0800

    > compile NEMU ysyx_22040905 程雨航 Linux agustin-RedmiBook-Pro-14S 5.15.0-25-generic #25-Ubuntu SMP Wed Mar 30 15:54:22 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  23:36:03 up  7:57,  1 user,  load average: 0.53, 0.46, 0.52

--- navy-apps/tests/bmp-test/main.c
+++ navy-apps/tests/bmp-test/main.c
@@ -6,7 +6,7 @@
 
 int main() {
   NDL_Init(0);
-  int w=0, h=0;
+  int w=100, h=100;
   NDL_OpenCanvas(&w, &h);
   printf("%d %d\n",w,h);
   /*void *bmp = BMP_Load("/share/pictures/projectn.bmp", &w, &h);

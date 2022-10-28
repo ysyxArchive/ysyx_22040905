d8717ad870cc02795b716f8a9f12f9cbbdcd557f
tracer-ysyx2204 <tracer@ysyx.org>
2022-10-29 04:21:49 +0800

    > compile NEMU ysyx_22040905 程雨航 Linux agustin-RedmiBook-Pro-14S 5.15.0-25-generic #25-Ubuntu SMP Wed Mar 30 15:54:22 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  04:21:49 up 10:11,  1 user,  load average: 0.41, 0.44, 0.41

--- nemu/src/monitor/monitor.c
+++ nemu/src/monitor/monitor.c
@@ -77,7 +77,7 @@ static void load_elf(){
     Assert(fp, "Can not open '%s'",elf[l]);
     fseek(fp,0L,SEEK_END);
     int file_size=ftell(fp);
-    assert(1==fread(buf, file_size, 1, fp));
+    assert(0!=fread(buf, file_size, 1, fp));
     fclose(fp);
     Elf64_Ehdr* ehdr=(Elf64_Ehdr*)buf;
     Elf64_Shdr* shdr=(Elf64_Shdr*)(buf+ehdr->e_shoff);

dde2d62eceb9708f8e6789195d59b08aed876218
tracer-ysyx2204 <tracer@ysyx.org>
2022-10-28 22:11:56 +0800

    > compile NEMU ysyx_22040905 程雨航 Linux agustin-RedmiBook-Pro-14S 5.15.0-25-generic #25-Ubuntu SMP Wed Mar 30 15:54:22 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  22:11:56 up  4:02,  1 user,  load average: 0.53, 0.51, 0.51

--- navy-apps/libs/libbmp/src/BMP.c
+++ navy-apps/libs/libbmp/src/BMP.c
@@ -37,7 +37,9 @@ void* BMP_Load(const char *filename, int *width, int *height) {
   int line_off = (w * 3 + 3) & ~0x3;
   for (int i = 0; i < h; i ++) {
     fseek(fp, hdr.offset + (h - 1 - i) * line_off, SEEK_SET);
+    printf("1\n");
     int nread = fread(&pixels[w * i], 3, w, fp);
+    printf("2\n");
     for (int j = w - 1; j >= 0; j --) {
       uint8_t b = *(((uint8_t*)&pixels[w * i]) + 3 * j);
       uint8_t g = *(((uint8_t*)&pixels[w * i]) + 3 * j + 1);
--- navy-apps/libs/libos/src/syscall.c
+++ navy-apps/libs/libos/src/syscall.c
@@ -75,7 +75,6 @@ void *_sbrk(intptr_t increment) {
 }
 
 int _read(int fd, void *buf, size_t count) {
-  printf("%ld %ld\n",(intptr_t)buf,count);
   return _syscall_(SYS_read,fd,(intptr_t)buf,(intptr_t)count);
 }
 

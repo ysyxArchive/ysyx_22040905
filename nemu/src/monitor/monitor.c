b596ddfb59b9c3f9776b06d83a73b9eddc5757fa
tracer-ysyx2204 <tracer@ysyx.org>
2022-10-29 03:15:52 +0800

    > compile NEMU ysyx_22040905 程雨航 Linux agustin-RedmiBook-Pro-14S 5.15.0-25-generic #25-Ubuntu SMP Wed Mar 30 15:54:22 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  03:15:52 up  9:06,  1 user,  load average: 0.30, 0.29, 0.34

--- nemu/src/monitor/monitor.c
+++ nemu/src/monitor/monitor.c
@@ -73,6 +73,10 @@ static void load_elf(){
     Elf64_Shdr shdr[2048];
     Elf64_Sym symtab[32768];
     char strtab[32768];
+    memset(ehdr,0,sizeof(ehdr));
+    memset(shdr,0,sizeof(shdr));
+    memset(symtab,0,sizeof(symtab));
+    memset(strtab,0,sizeof(strtab));
     FILE *fp = fopen(elf[l], "rb");
     Assert(fp, "Can not open '%s'",elf[l]);
     fseek(fp,0,SEEK_SET);
@@ -99,17 +103,16 @@ static void load_elf(){
       }
     }
     fclose(fp);
-      for(int j=0;j<num;j++){
-            if(ELF64_ST_TYPE(symtab[j].st_info)==STT_FUNC){
-              func[func_num[l]][l].begin=symtab[j].st_value;
-              func[func_num[l]][l].end=symtab[j].st_value+symtab[j].st_size;
-              func[func_num[l]++][l].str=strtab+symtab[j].st_name;
-              printf("%d %s\n",func_num[l]-1,func[func_num[l]-1][l].str);
-            }
+    for(int j=0;j<num;j++){
+      if(ELF64_ST_TYPE(symtab[j].st_info)==STT_FUNC){
+        func[func_num[l]][l].begin=symtab[j].st_value;
+        func[func_num[l]][l].end=symtab[j].st_value+symtab[j].st_size;
+        func[func_num[l]++][l].str=strtab+symtab[j].st_name;
       }
-   printf("***************************\n"); 
-  for(int i=0;i<func_num[l];i++){
-    printf("%d\t%s\n",i,func[i][l].str);}
+    }
+    printf("***************************\n"); 
+    for(int i=0;i<func_num[l];i++){
+      printf("%d\t%s\n",i,func[i][l].str);}
   }
   printf("###########################\n");
 for(int l=0;l<elf_num;l++)

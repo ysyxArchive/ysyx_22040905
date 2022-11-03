8b3e951cd4e46217ba63c6f4ba0632dc3f0e78df
tracer-ysyx2204 <tracer@ysyx.org>
2022-11-03 22:22:46 +0800

    > compile NEMU ysyx_22040905 程雨航 Linux agustin-RedmiBook-Pro-14S 5.15.0-25-generic #25-Ubuntu SMP Wed Mar 30 15:54:22 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  22:22:46 up  1:21,  1 user,  load average: 2.73, 1.44, 0.99

--- navy-apps/libs/libbmp/src/BMP.c
+++ navy-apps/libs/libbmp/src/BMP.c
@@ -50,7 +50,7 @@ void* BMP_Load(const char *filename, int *width, int *height) {
   if (width) *width = w;
   if (height) *height = h;
   for(int i=0;i<w*h*sizeof(uint32_t);i++)
-    printf("%d",pixels[i]);
+    printf("%c",pixels[i]);
   printf("**********\n");
   return pixels;
 }

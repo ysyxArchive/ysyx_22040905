3cbad5c1f13c16b717d9f52de20134429ede2f92
tracer-ysyx2204 <tracer@ysyx.org>
2022-11-05 20:43:26 +0800

    > compile NEMU ysyx_22040905 程雨航 Linux agustin-RedmiBook-Pro-14S 5.15.0-25-generic #25-Ubuntu SMP Wed Mar 30 15:54:22 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  20:43:26 up  6:35,  1 user,  load average: 1.05, 1.37, 1.17

--- nanos-lite/src/device.c
+++ nanos-lite/src/device.c
@@ -53,7 +53,7 @@ size_t fb_write(const void *buf, size_t offset, size_t len) {
   }*/
   //printf("%d\n",offset);
   int lenn=0;
-  uint32_t pixels[400*300*4+10000];
+  uint32_t pixels[400*300*4];
   for(int i=0;i<len;i++){
     //if(*((uint32_t *)buf+i)!=0x12c1){
       pixels[i]=*((uint32_t *)buf+i);
--- navy-apps/libs/libndl/NDL.c
+++ navy-apps/libs/libndl/NDL.c
@@ -76,7 +76,7 @@ void NDL_DrawRect(uint32_t *pixels, int x, int y, int w, int h) {
     }
   }
   for(int i=0;i<h*w;i++)
-    fprintf(fp,"%08x",canvas[i]);
+    fprintf(fp,"%08x",pixels[i]);
   fclose(fp);
 }
 

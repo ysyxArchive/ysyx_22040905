b78e52c11e2316fa66ea4ce3067305a6735102a5
tracer-ysyx2204 <tracer@ysyx.org>
2022-11-05 20:47:52 +0800

    > compile NEMU ysyx_22040905 程雨航 Linux agustin-RedmiBook-Pro-14S 5.15.0-25-generic #25-Ubuntu SMP Wed Mar 30 15:54:22 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  20:47:52 up  6:39,  1 user,  load average: 1.64, 1.50, 1.28

--- navy-apps/libs/libndl/NDL.c
+++ navy-apps/libs/libndl/NDL.c
@@ -76,7 +76,7 @@ void NDL_DrawRect(uint32_t *pixels, int x, int y, int w, int h) {
     }
   }
   for(int i=0;i<canvas_h*canvas_w;i++)
-    fprintf(fp,"%08x",canvas[i]);
+    {fprintf(fp,"%08x",canvas[i]);printf("%d\n",i);}
   fclose(fp);
 }
 

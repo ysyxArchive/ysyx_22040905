85b42644c366a15b45f558f0889dc4b920607e72
tracer-ysyx2204 <tracer@ysyx.org>
2022-11-05 22:21:31 +0800

    > compile NEMU ysyx_22040905 程雨航 Linux agustin-RedmiBook-Pro-14S 5.15.0-25-generic #25-Ubuntu SMP Wed Mar 30 15:54:22 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  22:21:31 up  8:13,  1 user,  load average: 2.00, 1.10, 1.17

--- nanos-lite/src/device.c
+++ nanos-lite/src/device.c
@@ -53,10 +53,10 @@ size_t fb_write(const void *buf, size_t offset, size_t len) {
   }
   //printf("%d:%d\n",++count,lenn);
   ctl.pixels=pixels;
-  ctl.y=offset/cfg.width;
-  ctl.x=offset%cfg.width;
-  ctl.h=(offset+len/4)/cfg.width;
-  ctl.w=(offset+len/4)%cfg.width;
+  ctl.y=offset/4/cfg.width;
+  ctl.x=offset/4%cfg.width;
+  ctl.h=(offset+len)/4/cfg.width;
+  ctl.w=(offset+len)/4%cfg.width;
   assert(ctl.y<300&&ctl.x<400&&ctl.h<300&&ctl.w<400);
   ctl.sync=1;
   ioe_read(AM_GPU_FBDRAW,&ctl);

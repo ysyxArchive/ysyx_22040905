6661705cdbb595b6358aa0963331f350553d54e6
tracer-ysyx2204 <tracer@ysyx.org>
2022-11-06 14:45:00 +0800

    > compile NEMU ysyx_22040905 程雨航 Linux agustin-RedmiBook-Pro-14S 5.15.0-25-generic #25-Ubuntu SMP Wed Mar 30 15:54:22 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  14:45:00 up  2:26,  1 user,  load average: 1.14, 0.66, 0.56

--- navy-apps/tests/fixe-test/main.c
+++ navy-apps/tests/fixe-test/main.c
@@ -8,18 +8,24 @@ int main() {
   fixedpt_muli(fixedpt_rconst(1.2),10);
   fixedpt_muli(fixedpt_rconst(-1.2),10);
   fixedpt_muli(fixedpt_rconst(0),5);
-
+  printf("\n");
   fixedpt_divi(fixedpt_rconst(4.8),2);
   fixedpt_divi(fixedpt_rconst(2),5);
   fixedpt_divi(fixedpt_rconst(4.8),-2);
   fixedpt_divi(fixedpt_rconst(-2),5);
   fixedpt_divi(fixedpt_rconst(0),-2);
-
+  printf("\n");
   fixedpt_mul(fixedpt_rconst(1.2),fixedpt_rconst(1.2));
   fixedpt_mul(fixedpt_rconst(-1.2),fixedpt_rconst(1.2));
   fixedpt_mul(fixedpt_rconst(-1.2),fixedpt_rconst(-1.2));
   fixedpt_mul(fixedpt_rconst(0),fixedpt_rconst(0));
-
+  printf("\n");
+  fixedpt_div(fixedpt_rconst(4.8),fixedpt_rconst(2));
+  fixedpt_div(fixedpt_rconst(2),fixedpt_rconst(5));
+  fixedpt_div(fixedpt_rconst(4.8),fixedpt_rconst(-2));
+  fixedpt_div(fixedpt_rconst(-2),fixedpt_rconst(5));
+  fixedpt_div(fixedpt_rconst(0),fixedpt_rconst(-2));
+  printf("\n");
 
   return 0;
 }

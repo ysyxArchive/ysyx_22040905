3c665c01e594535e1c569af97c1486e191b3a1a7
tracer-ysyx2204 <tracer@ysyx.org>
2022-11-06 14:22:26 +0800

    > compile NEMU ysyx_22040905 程雨航 Linux agustin-RedmiBook-Pro-14S 5.15.0-25-generic #25-Ubuntu SMP Wed Mar 30 15:54:22 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  14:22:26 up  2:03,  1 user,  load average: 1.64, 0.91, 0.70

--- navy-apps/libs/libfixedptc/include/fixedptc.h
+++ navy-apps/libs/libfixedptc/include/fixedptc.h
@@ -126,43 +126,45 @@ typedef	__uint128_t fixedptud;
  * Putting them only in macros will effectively make them optional. */
 #define fixedpt_tofloat(T) ((float) ((T)*((float)(1)/(float)(1L << FIXEDPT_FBITS))))
 
+static inline char* fixedpt_cstr(const fixedpt A, const int max_dec);
+
 /* Multiplies a fixedpt number with an integer, returns the result. */
 static inline fixedpt fixedpt_muli(fixedpt A, int B) {
-	//printf("muli:%d %d %d\n",fixedpt_toint(A),B,fixedpt_toint(A*B));
+	printf("muli:%s %d %s\n",fixedpt_cstr(A,-2),B,fixedpt_cstr(A*B,-2));
 	return A*B;
 }
 
 /* Divides a fixedpt number with an integer, returns the result. */
 static inline fixedpt fixedpt_divi(fixedpt A, int B) {
-	//printf("divi:%d %d %d\n",fixedpt_toint(A),B,fixedpt_toint(A/B));
+	printf("divi:%s %d %s\n",fixedpt_cstr(A,-2),B,fixedpt_cstr(A/B,-2));
 	return A/B;
 }
 
 /* Multiplies two fixedpt numbers, returns the result. */
 static inline fixedpt fixedpt_mul(fixedpt A, fixedpt B) {
-	printf("mul:%d %d %d\n",A,B,(A*B)>>FIXEDPT_FBITS);
+	printf("mul:%s %s %s\n",fixedpt_cstr(A,-2),fixedpt_cstr(B,-2),fixedpt_cstr((A*B)>>FIXEDPT_FBITS,-2));
 	return (A*B)>>FIXEDPT_FBITS;
 }
 
 
 /* Divides two fixedpt numbers, returns the result. */
 static inline fixedpt fixedpt_div(fixedpt A, fixedpt B) {
-	//printf("div:%d %d %d\n",fixedpt_toint(A),fixedpt_toint(B),fixedpt_toint((A/B)<<FIXEDPT_FBITS));
+	printf("div:%s %s %s\n",fixedpt_cstr(A,-2),fixedpt_cstr(B,-2),fixedpt_cstr((A/B)<<FIXEDPT_FBITS,-2));
 	return (A/B)<<FIXEDPT_FBITS;
 }
 
 static inline fixedpt fixedpt_abs(fixedpt A) {
-	//printf("abs:%d %d\n",fixedpt_toint(A),fixedpt_toint((A>>31==1)?(~A+1):A));
+	printf("abs:%s %s\n",fixedpt_cstr(A,-2),fixedpt_cstr((A>>31==1)?(~A+1):A,-2));
 	return (A>>31==1)?(~A+1):A;
 }
 
 static inline fixedpt fixedpt_floor(fixedpt A) {
-	//printf("floor:%d %d\n",fixedpt_toint(A),fixedpt_toint(A/(1<<FIXEDPT_FBITS)));
+	printf("floor:%s %s\n",fixedpt_cstr(A,-2),fixedpt_cstr(A/(1<<FIXEDPT_FBITS),-2));
 	return A/(1<<FIXEDPT_FBITS);
 }
 
 static inline fixedpt fixedpt_ceil(fixedpt A) {
-	//printf("ceil:%d %d\n",fixedpt_toint(A),fixedpt_toint((A%(1<<FIXEDPT_FBITS)==0)?A:A/(1<<FIXEDPT_FBITS)+(1<<FIXEDPT_FBITS)));
+	printf("ceil:%s %s\n",fixedpt_cstr(A,-2),fixedpt_cstr((A%(1<<FIXEDPT_FBITS)==0)?A:A/(1<<FIXEDPT_FBITS)+(1<<FIXEDPT_FBITS),-2));
 	return (A%(1<<FIXEDPT_FBITS)==0)?A:A/(1<<FIXEDPT_FBITS)+(1<<FIXEDPT_FBITS);
 }
 

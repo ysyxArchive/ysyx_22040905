0e2d2222fb77268d2dcf39cbc8d692e732fe2fb4
tracer-ysyx2204 <tracer@ysyx.org>
2022-11-06 14:56:22 +0800

    > compile NEMU ysyx_22040905 程雨航 Linux agustin-RedmiBook-Pro-14S 5.15.0-25-generic #25-Ubuntu SMP Wed Mar 30 15:54:22 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  14:56:22 up  2:37,  1 user,  load average: 0.70, 0.84, 0.72

--- navy-apps/libs/libfixedptc/include/fixedptc.h
+++ navy-apps/libs/libfixedptc/include/fixedptc.h
@@ -169,8 +169,8 @@ static inline fixedpt fixedpt_abs(fixedpt A) {
 
 static inline fixedpt fixedpt_floor(fixedpt A) {
 	printf("floor:%s ",fixedpt_cstr(A,-2));
-	printf("%s\n",fixedpt_cstr(A&(1<<FIXEDPT_FBITS),-2));
-	return A&(1<<FIXEDPT_FBITS);
+	printf("%s\n",fixedpt_cstr(A-fixedpt_fracpart(A),-2));
+	return (A-fixedpt_fracpart(A));
 }
 
 static inline fixedpt fixedpt_ceil(fixedpt A) {

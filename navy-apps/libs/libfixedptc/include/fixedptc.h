6920bc234d0c1ee0598897a8cd1a7c653ae7559b
tracer-ysyx2204 <tracer@ysyx.org>
2022-11-06 13:27:59 +0800

    > compile NEMU ysyx_22040905 程雨航 Linux agustin-RedmiBook-Pro-14S 5.15.0-25-generic #25-Ubuntu SMP Wed Mar 30 15:54:22 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  13:27:59 up  1:09,  1 user,  load average: 0.71, 0.45, 0.40

--- navy-apps/libs/libfixedptc/include/fixedptc.h
+++ navy-apps/libs/libfixedptc/include/fixedptc.h
@@ -128,41 +128,41 @@ typedef	__uint128_t fixedptud;
 
 /* Multiplies a fixedpt number with an integer, returns the result. */
 static inline fixedpt fixedpt_muli(fixedpt A, int B) {
-	printf("muli:%d %d %d\n",A,B,A*B);
+	printf("muli:%d %d %d\n",fixedpt_toint(A),B,fixedpt_toint(A*B));
 	return A*B;
 }
 
 /* Divides a fixedpt number with an integer, returns the result. */
 static inline fixedpt fixedpt_divi(fixedpt A, int B) {
-	printf("divi:%d %d %d\n",A,B,A/B);
+	printf("divi:%d %d %d\n",fixedpt_toint(A),B,fixedpt_toint(A/B));
 	return A/B;
 }
 
 /* Multiplies two fixedpt numbers, returns the result. */
 static inline fixedpt fixedpt_mul(fixedpt A, fixedpt B) {
-	printf("mul:%d %d %d\n",A,B,(A*B)>>FIXEDPT_FBITS);
+	printf("mul:%d %d %d\n",fixedpt_toint(A),fixedpt_toint(B),fixedpt_toint((A*B)>>FIXEDPT_FBITS));
 	return (A*B)>>FIXEDPT_FBITS;
 }
 
 
 /* Divides two fixedpt numbers, returns the result. */
 static inline fixedpt fixedpt_div(fixedpt A, fixedpt B) {
-	printf("div:%d %d %d\n",A,B,(A/B)<<FIXEDPT_FBITS);
+	printf("div:%d %d %d\n",fixedpt_toint(A),fixedpt_toint(B),fixedpt_toint((A/B)<<FIXEDPT_FBITS));
 	return (A/B)<<FIXEDPT_FBITS;
 }
 
 static inline fixedpt fixedpt_abs(fixedpt A) {
-	printf("abs:%d %d\n",A,(A>>31==1)?(~A+1):A);
+	printf("abs:%d %d\n",fixedpt_toint(A),fixedpt_toint((A>>31==1)?(~A+1):A));
 	return (A>>31==1)?(~A+1):A;
 }
 
 static inline fixedpt fixedpt_floor(fixedpt A) {
-	printf("floor:%d %d\n",A,A/(1<<FIXEDPT_FBITS));
+	printf("floor:%d %d\n",fixedpt_toint(A),fixedpt_toint(A/(1<<FIXEDPT_FBITS)));
 	return A/(1<<FIXEDPT_FBITS);
 }
 
 static inline fixedpt fixedpt_ceil(fixedpt A) {
-	printf("ceil:%d %d\n",A,(A%(1<<FIXEDPT_FBITS)==0)?A:A/(1<<FIXEDPT_FBITS)+(1<<FIXEDPT_FBITS));
+	printf("ceil:%d %d\n",fixedpt_toint(A),fixedpt_toint((A%(1<<FIXEDPT_FBITS)==0)?A:A/(1<<FIXEDPT_FBITS)+(1<<FIXEDPT_FBITS)));
 	return (A%(1<<FIXEDPT_FBITS)==0)?A:A/(1<<FIXEDPT_FBITS)+(1<<FIXEDPT_FBITS);
 }
 

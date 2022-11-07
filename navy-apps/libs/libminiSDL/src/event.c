8ac9dd80385dbaff63535655a2757e1c64ba8256
tracer-ysyx2204 <tracer@ysyx.org>
2022-11-07 15:12:03 +0800

    > compile NEMU ysyx_22040905 程雨航 Linux agustin-RedmiBook-Pro-14S 5.15.0-25-generic #25-Ubuntu SMP Wed Mar 30 15:54:22 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  15:12:03 up 12:45,  1 user,  load average: 0.46, 1.17, 1.02

--- navy-apps/libs/libminiSDL/src/event.c
+++ navy-apps/libs/libminiSDL/src/event.c
@@ -17,10 +17,11 @@ int SDL_PollEvent(SDL_Event *ev) {
 }
 
 int SDL_WaitEvent(SDL_Event *event) {
-  static char buf[2];
-  NDL_PollEvent(buf, 2);
+  static char buf[20];
+  NDL_PollEvent(buf, 20);
   event->key.type=(int)buf[0];
   event->key.keysym.sym=(int)buf[1];
+  printf("buf:%s\n",buf);
   printf("%d %d\n",event->key.type,event->key.keysym.sym);
   return 1;
 }

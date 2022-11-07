9b2dbe1e28e0af7c4af8195ccb257f0625150704
tracer-ysyx2204 <tracer@ysyx.org>
2022-11-07 14:37:05 +0800

    > compile NEMU ysyx_22040905 程雨航 Linux agustin-RedmiBook-Pro-14S 5.15.0-25-generic #25-Ubuntu SMP Wed Mar 30 15:54:22 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  14:37:05 up 12:10,  1 user,  load average: 1.04, 0.86, 0.85

--- nanos-lite/src/device.c
+++ nanos-lite/src/device.c
@@ -58,7 +58,7 @@ size_t fb_write(const void *buf, size_t offset, size_t len) {
   ctl.x=offset/4%cfg.width;
   ctl.h=(offset+len)/4/cfg.width-ctl.y;
   ctl.w=((offset+len)/4-1)%cfg.width+1 -ctl.x;
-  //printf("%d %d %d %d\n",ctl.x,ctl.y,ctl.w,ctl.h);
+  printf("%d %d %d %d\n",ctl.x,ctl.y,ctl.w,ctl.h);
   if(!(ctl.y<300&&ctl.x<400&&(ctl.h+ctl.y)<=300&&(ctl.w+ctl.x)<=400)){
     printf("x:%d y:%d w:%d h:%d\n",ctl.x,ctl.y,ctl.w,ctl.h);
     assert(0);

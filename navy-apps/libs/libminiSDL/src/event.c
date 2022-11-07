35ee35bd10e5b675c63cd05e17ec06e206217c2d
tracer-ysyx2204 <tracer@ysyx.org>
2022-11-07 18:50:58 +0800

    > compile NEMU ysyx_22040905 程雨航 Linux agustin-RedmiBook-Pro-14S 5.15.0-25-generic #25-Ubuntu SMP Wed Mar 30 15:54:22 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  18:50:58 up 16:24,  1 user,  load average: 1.17, 0.54, 0.51

--- navy-apps/libs/libminiSDL/src/event.c
+++ navy-apps/libs/libminiSDL/src/event.c
@@ -3,6 +3,22 @@
 #include <string.h>
 #define keyname(k) #k,
 
+#define ElemType int
+#define MAXSIZE 2048
+typedef struct Queue
+{
+	ElemType *base; 
+	int       front; 
+	int       rear; 
+}Queue;
+
+void InitQueue(Queue *Q)
+{
+	Q->base = (ElemType *)malloc(sizeof(ElemType) * MAXSIZE);
+	assert(Q->base != NULL);
+	Q->front = Q->rear = 0;
+}
+
 static const char *keyname[] = {
   "NONE",
   _KEYS(keyname)
@@ -13,14 +29,17 @@ int SDL_PushEvent(SDL_Event *ev) {
 }
 
 int SDL_PollEvent(SDL_Event *ev) {
-  return 0;
+  static char buf[20];
+  NDL_PollEvent(buf, 20);
+  if(buf==NULL) return 0;
+  ev->key.type=(buf[1]=='u'?1:0);
+  ev->key.keysym.sym=(int)buf[3];
+  return 1;
 }
 
 int SDL_WaitEvent(SDL_Event *event) {
-  static char buf[20];
-  NDL_PollEvent(buf, 20);
-  event->key.type=(buf[1]=='u'?1:0);
-  event->key.keysym.sym=(int)buf[3];
+  printf("Finish\n");
+  while(!SDL_PollEvent(event));
   //printf("%d %d\n",event->key.type,event->key.keysym.sym);
   return 1;
 }
@@ -30,5 +49,6 @@ int SDL_PeepEvents(SDL_Event *ev, int numevents, int action, uint32_t mask) {
 }
 
 uint8_t* SDL_GetKeyState(int *numkeys) {
+
   return NULL;
 }

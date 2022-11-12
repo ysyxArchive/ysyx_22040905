ff00facf191b16e882b3a0a1284e09348112b808
tracer-ysyx2204 <tracer@ysyx.org>
2022-11-12 15:36:59 +0800

    > compile NEMU ysyx_22040905 程雨航 Linux agustin-RedmiBook-Pro-14S 5.15.0-25-generic #25-Ubuntu SMP Wed Mar 30 15:54:22 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  15:36:59 up 12 min,  1 user,  load average: 0.99, 0.92, 0.47

--- nemu/src/monitor/monitor.c
+++ nemu/src/monitor/monitor.c
@@ -191,7 +191,7 @@ void init_monitor(int argc, char *argv[]) {
 
   /* Initialize differential testing. */
   init_difftest(diff_so_file, img_size, difftest_port);
-
+  return ;
   /* Initialize the simple debugger. */
   init_sdb();
 

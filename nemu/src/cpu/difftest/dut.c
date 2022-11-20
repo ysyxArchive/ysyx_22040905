44ad653f115b9f28c1af838adcb31ac4a441b951
tracer-ysyx2204 <tracer@ysyx.org>
2022-11-20 15:56:58 +0800

    > compile NEMU ysyx_22040905 程雨航 Linux agustin-RedmiBook-Pro-14S 5.15.0-25-generic #25-Ubuntu SMP Wed Mar 30 15:54:22 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  15:56:58 up  3:51,  1 user,  load average: 0.42, 0.76, 0.72

--- nemu/src/cpu/difftest/dut.c
+++ nemu/src/cpu/difftest/dut.c
@@ -134,10 +134,7 @@ void difftest_attach(){
   ref_difftest_regcpy(&cpuu,DIFFTEST_TO_REF);
   //execute
   ref_difftest_exec(len/4);
-  for(int i=0;i<16;i++)
-  {
-    //printf("%x\n",buff[i]);
-  }
+  printf("%lx\n",RESET_VECTOR+img_size_2);
   //change gpr
   ref_difftest_memcpy(RESET_VECTOR+0x100000, guest_to_host(RESET_VECTOR+0x100000), img_size_2-0x100000, DIFFTEST_TO_REF);
   ref_difftest_regcpy(&cpu, DIFFTEST_TO_REF);

973e96d7a86a58b37bc789bb19515eafaac9ba11
tracer-ysyx2204 <tracer@ysyx.org>
2022-11-20 13:17:52 +0800

    > compile NEMU ysyx_22040905 程雨航 Linux agustin-RedmiBook-Pro-14S 5.15.0-25-generic #25-Ubuntu SMP Wed Mar 30 15:54:22 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  13:17:52 up  1:12,  1 user,  load average: 0.55, 0.44, 0.37

--- nemu/src/cpu/difftest/dut.c
+++ nemu/src/cpu/difftest/dut.c
@@ -90,6 +90,7 @@ static void checkregs(CPU_state *ref, vaddr_t pc) {
 
 void difftest_step(vaddr_t pc, vaddr_t npc) {
   if(!is_difftest_mode)return ;
+  printf("1\n");
   CPU_state ref_r;
 
   if (skip_dut_nr_inst > 0) {
@@ -110,7 +111,7 @@ void difftest_step(vaddr_t pc, vaddr_t npc) {
     ref_difftest_regcpy(&cpu, DIFFTEST_TO_REF);
     is_skip_ref = false;
     return;
-  }
+  is_difftest_mode = true;}
 
   ref_difftest_exec(1);
   ref_difftest_regcpy(&ref_r, DIFFTEST_TO_DUT);

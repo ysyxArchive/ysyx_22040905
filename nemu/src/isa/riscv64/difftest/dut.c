cf2add34d394ecef128e38d252308fd3de1c6f63
tracer-ysyx2204 <tracer@ysyx.org>
2022-11-20 13:42:56 +0800

    > compile NEMU ysyx_22040905 程雨航 Linux agustin-RedmiBook-Pro-14S 5.15.0-25-generic #25-Ubuntu SMP Wed Mar 30 15:54:22 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  13:42:56 up  1:37,  1 user,  load average: 1.04, 0.59, 0.56

--- nemu/src/isa/riscv64/difftest/dut.c
+++ nemu/src/isa/riscv64/difftest/dut.c
@@ -9,10 +9,11 @@ bool isa_difftest_checkregs(CPU_state *ref_r, vaddr_t pc) {
   {
     if(!difftest_check_reg(reg_name(i,64),pc,ref_r->gpr[i],gpr(i)))flag=0;
   }
+  printf("%ld %ld\n",ref_r->pc,ref_r->mepc);
   if(flag) return true;
   return false;
 }
 
 void isa_difftest_attach() {
-;
+
 }

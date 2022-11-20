b3b2cbee88c6464fe65b188ae417eeb01d630560
tracer-ysyx2204 <tracer@ysyx.org>
2022-11-20 21:39:23 +0800

    > compile NEMU ysyx_22040905 程雨航 Linux agustin-RedmiBook-Pro-14S 5.15.0-25-generic #25-Ubuntu SMP Wed Mar 30 15:54:22 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  21:39:23 up  9:34,  1 user,  load average: 0.58, 0.52, 0.58

--- nemu/src/isa/riscv64/difftest/dut.c
+++ nemu/src/isa/riscv64/difftest/dut.c
@@ -54,7 +54,12 @@ void isa_difftest_attach(uint32_t *buf,int *len) {
   buf[28]=(0x713)|(BITS(cpu.mcause,7,0)<<20);    //addi  a4,a4,num[7,0]
   buf[29]=0x34271073;                        //csrrw $0,mcause,a4
 
-/*
+  // set mstatus.MPRV to pass difftest
+  buf[30]=(0x737)|(20<<12);                  //lui a0, (1 << (17-12))
+  buf[31]=0xa36333;                          //or t1, t1, a0
+  buf[32]=0x30031073;                        //csrrw $0,mstatus, t1
+
+  /*
   buf[30]=0x00000713;                        //addi  a4,$0,0
   buf[31]=(0x737)|(BITS(cpu.mtvec,63,44)<<12);  //lui   a4,num[63,44]
   buf[32]=(0x713)|(BITS(cpu.mtvec,43,32)<<20);  //addi  a4,a4,num[43,32]

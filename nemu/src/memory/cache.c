f392d911495445e8dd33db7c920cef731230515f
tracer-ysyx2204 <tracer@ysyx.org>
2023-04-09 01:24:51 +0800

    > compile NEMU ysyx_22040905 程雨航 Linux cyh-RedmiBook-Pro-14S 5.15.0-25-generic #25-Ubuntu SMP Wed Mar 30 15:54:22 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  01:24:51 up 5 days,  3:52,  1 user,  load average: 1.17, 1.07, 1.17

--- nemu/src/memory/cache.c
+++ nemu/src/memory/cache.c
@@ -57,8 +57,6 @@ word_t cache_read(uintptr_t addr,size_t len)
     //printf("d\n");
     for(int i=0;i<BLOCK_SIZE/8;i++)
         pmem_write(((cache_tag[way2][idx]<<idx_width |idx)<<offset_width) | (i*8),8,*(cache_data[way2][idx]+(i*8)));
-    if(BLOCK_SIZE%8!=0) 
-        pmem_write(((cache_tag[way2][idx]<<idx_width |idx)<<offset_width) | (BLOCK_SIZE/8*8),BLOCK_SIZE%8,*(cache_data[way2][idx]+(BLOCK_SIZE/8*8)));
     D[way2][idx] = 0;
   }//else printf("ud\n");
 
@@ -106,8 +104,6 @@ void cache_write(uintptr_t addr, size_t len, word_t data)
     //printf("dirty\n");
     for(int i=0;i<BLOCK_SIZE/8;i++)
         pmem_write(((cache_tag[way2][idx]<<idx_width |idx)<<offset_width) | (i*8),8,*(cache_data[way2][idx]+(i*8)));
-    if(BLOCK_SIZE%8!=0) 
-        pmem_write(((cache_tag[way2][idx]<<idx_width |idx)<<offset_width) | (BLOCK_SIZE/8*8),BLOCK_SIZE%8,*(cache_data[way2][idx]+(BLOCK_SIZE/8*8)));
     D[way2][idx] = 0;
    }//else printf("ud\n");
 
@@ -118,13 +114,11 @@ void cache_write(uintptr_t addr, size_t len, word_t data)
 
     assert(offset+len<=BLOCK_SIZE);
     
-    printf("%lx %lx\n",*(word_t *)(buf),*(word_t *)(buf+ 8));
     host_write(buf+offset,len,data);
-    printf("%lx %lx\n",*(word_t *)(buf),*(word_t *)(buf+ 8));
     for (int i = 0; i < line_size; i++){
         cache_data[way2][idx][i] = buf[i];
     }
-    printf("%lx %lx\n",*(word_t *)(cache_data[way2][idx]),*(word_t *)(cache_data[way2][idx]+ 8));
+    //printf("%lx %lx\n",*(word_t *)(cache_data[way2][idx]),*(word_t *)(cache_data[way2][idx]+ 8));
 
     cache_tag[way2][idx] = tag;
 

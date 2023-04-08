1ab5d7cc5223c35a841d855e4c3e6dacf98c4589
tracer-ysyx2204 <tracer@ysyx.org>
2023-04-09 00:35:00 +0800

    > compile NEMU ysyx_22040905 程雨航 Linux cyh-RedmiBook-Pro-14S 5.15.0-25-generic #25-Ubuntu SMP Wed Mar 30 15:54:22 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  00:35:00 up 5 days,  3:02,  1 user,  load average: 1.33, 0.96, 1.06

--- nemu/src/memory/cache.c
+++ nemu/src/memory/cache.c
@@ -74,8 +74,7 @@ word_t cache_read(uintptr_t addr,size_t len)
   }
   cache_tag[way2][idx] = tag;
   V[way2][idx] = 1;
-  printf("miss\n");
-  //printf("%lx %lx\n",*(word_t *)(cache_data[way2][idx]),*(word_t *)(cache_data[way2][idx] + 8));
+  printf("%lx %lx\n",*(word_t *)(cache_data[way2][idx]),*(word_t *)(cache_data[way2][idx] + 8));
   return *(word_t *)(cache_data[way2][idx] + offset);
 }
 

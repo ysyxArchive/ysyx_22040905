7b6460594762a5c264b4ddb5a28b15bb8218a28c
tracer-ysyx2204 <tracer@ysyx.org>
2023-04-08 21:16:57 +0800

    > compile NEMU ysyx_22040905 程雨航 Linux cyh-RedmiBook-Pro-14S 5.15.0-25-generic #25-Ubuntu SMP Wed Mar 30 15:54:22 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  21:16:57 up 4 days, 23:44,  1 user,  load average: 0.24, 0.65, 0.86

--- nemu/src/memory/paddr.c
+++ nemu/src/memory/paddr.c
@@ -15,7 +15,7 @@ uint8_t* guest_to_host(paddr_t paddr) { return pmem + paddr - CONFIG_MBASE; }
 paddr_t host_to_guest(uint8_t *haddr) { return haddr - pmem + CONFIG_MBASE; }
 
 /*static*/ word_t pmem_read(paddr_t addr, int len) {
-  printf("%x %d\n",addr,len);
+  printf("%lx %d\n",guest_to_host(addr)-pmem,len);
   word_t ret = host_read(guest_to_host(addr), len);
   return ret;
 }

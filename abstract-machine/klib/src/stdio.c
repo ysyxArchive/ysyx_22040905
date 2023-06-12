e434debd84585e9d1ff3a27c92d8f6f4ed16e4f2
tracer-ysyx2204 <tracer@ysyx.org>
2023-06-13 01:17:36 +0800

    > compile NEMU ysyx_22040905 程雨航 Linux cyh-RedmiBook-Pro-14S 5.15.0-25-generic #25-Ubuntu SMP Wed Mar 30 15:54:22 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  01:17:36 up 22:50,  1 user,  load average: 0.19, 0.36, 0.26

--- abstract-machine/klib/src/stdio.c
+++ abstract-machine/klib/src/stdio.c
@@ -25,50 +25,6 @@ size_t strnlen(const char * s, size_t count)
 	return sc - s;
 }
 
-uint32_t __attribute__((weak)) __div64_32(void *nn, uint32_t base)
-{
-  uint64_t *n=(uint64_t *)nn;
-	uint64_t rem = *n;
-	uint64_t b = base;
-	uint64_t res, d = 1;
-	uint32_t high = rem >> 32;
-
-	/* Reduce the thing a bit first */
-	res = 0;
-	if (high >= base) {
-		high /= base;
-		res = (uint64_t) high << 32;
-		rem -= (uint64_t) (high*base) << 32;
-	}
-
-	while ((int64_t)b > 0 && b < rem) {
-		b = b+b;
-		d = d+d;
-	}
-
-	do {
-		if (rem >= b) {
-			rem -= b;
-			res += d;
-		}
-		b >>= 1;
-		d >>= 1;
-	} while (d);
-
-	*n = res;
-	return rem;
-}
-
-//进制之间的相应转换
-# define do_div(n,base) ({						\
-	unsigned int __base = (base);					\
-	unsigned int __rem;	\
-	(void)(((typeof((n)) *)0) == ((unsigned long long *)0));	\
-	if (((n) >> 32) == 0) {						\
-	} else								\
-		__rem = __div64_32(&(n), __base);			\
-	__rem;								\
- })
 
 static int skip_atoi(const char **s)
 {

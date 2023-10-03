e391ba385babcc988fbd7c70f412f85dfd341c2c
tracer-ysyx2204 <tracer@ysyx.org>
2023-10-04 02:41:18 +0800

    > compile NEMU ysyx_22040905 程雨航 Linux cyh-RedmiBook-Pro-14S 5.15.0-25-generic #25-Ubuntu SMP Wed Mar 30 15:54:22 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  02:41:18 up 10:57,  1 user,  load average: 1.29, 0.95, 1.07

--- abstract-machine/am/src/riscv/nemu/cte.c
+++ abstract-machine/am/src/riscv/nemu/cte.c
@@ -56,8 +56,8 @@ void iset(bool enable) {
     //asm volatile("csrsi mstatus, 8"); 
 //mie_MTIP
 
-    //asm volatile("li t0, 0x80");
-    asm volatile("csrrs x0,mie, t0");  
+    asm volatile("li t0, 0x80");
+    //asm volatile("csrrs x0,mie, t0");  
   }
   else{
     //asm volatile("csrci mstatus, 8"); 

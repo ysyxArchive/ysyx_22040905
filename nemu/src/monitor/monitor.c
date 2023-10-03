3f74b70443157c94a16d0e91361397986a88c8fd
tracer-ysyx2204 <tracer@ysyx.org>
2023-10-03 15:48:12 +0800

    > compile NEMU ysyx_22040905 程雨航 Linux cyh-RedmiBook-Pro-14S 5.15.0-25-generic #25-Ubuntu SMP Wed Mar 30 15:54:22 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  15:48:12 up 4 min,  1 user,  load average: 0.42, 0.41, 0.19

--- nemu/src/monitor/monitor.c
+++ nemu/src/monitor/monitor.c
@@ -8,7 +8,9 @@ void init_mem();
 void init_difftest(char *ref_so_file, long img_size, int port);
 void init_device();
 void init_sdb();
+#ifdef CONFIG_ITRACE
 void init_disasm(const char *triple);
+#endif
 
 static void welcome() {
   Log("Trace: %s", MUXDEF(CONFIG_TRACE, ASNI_FMT("ON", ASNI_FG_GREEN), ASNI_FMT("OFF", ASNI_FG_RED)));

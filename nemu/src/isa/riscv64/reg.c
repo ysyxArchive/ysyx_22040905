e74d6a03a0a9145084ba14ad536caca97ca82492
tracer-ysyx2204 <tracer@ysyx.org>
2023-10-04 03:18:03 +0800

    > compile NEMU ysyx_22040905 程雨航 Linux cyh-RedmiBook-Pro-14S 5.15.0-25-generic #25-Ubuntu SMP Wed Mar 30 15:54:22 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  03:18:03 up 11:34,  1 user,  load average: 0.51, 0.83, 1.05

--- nemu/src/isa/riscv64/reg.c
+++ nemu/src/isa/riscv64/reg.c
@@ -13,7 +13,7 @@ void isa_reg_display() {
   for(int i=0;i<32;i++){
     printf("%s:%016lx\n",reg_name(i,8),gpr(i));
   }
-  printf("cpu.mcause:%016lx\ncpu.mstatus:%016lx\ncpu.mepc:%016lx\n",cpu.mcause,cpu.mstatus, cpu.mepc);
+  printf("cpu.mcause:%016lx\ncpu.mstatus:%016lx\ncpu.mepc:%016lx\ncpu.mip:%016lx\ncpu.mie:%016lx\n",cpu.mcause,cpu.mstatus, cpu.mepc,cpu.mip,cpu.mie);
 }
 
 word_t isa_reg_str2val(const char *s, bool *success) {

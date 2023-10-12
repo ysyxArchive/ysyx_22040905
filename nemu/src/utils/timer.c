327b503108fc1e4621a9be5e185ff4c93f10a9ab
tracer-ysyx2204 <tracer@ysyx.org>
2023-10-12 20:15:19 +0800

    > compile NEMU ysyx_22040905 程雨航 Linux cyh-RedmiBook-Pro-14S 5.15.0-25-generic #25-Ubuntu SMP Wed Mar 30 15:54:22 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  20:15:19 up  4:15,  5 users,  load average: 0.82, 0.83, 0.70

--- nemu/src/utils/timer.c
+++ nemu/src/utils/timer.c
@@ -9,13 +9,14 @@ IFDEF(CONFIG_TIMER_CLOCK_GETTIME,
 static uint64_t boot_time = 0;
 
 struct timeval now;
+uint64_t us;
 
 static uint64_t get_time_internal() {
 #if defined(CONFIG_TARGET_AM)
   uint64_t us = io_read(AM_TIMER_UPTIME).us;
 #elif defined(CONFIG_TIMER_GETTIMEOFDAY)
   gettimeofday(&now, NULL);
-  uint64_t us = now.tv_sec * 1000000 + now.tv_usec;
+  us = now.tv_sec * 1000000 + now.tv_usec;
 #else
   struct timespec now;
   clock_gettime(CLOCK_MONOTONIC_COARSE, &now);

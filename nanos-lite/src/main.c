0492adb0ca16d52c602d4165cfce2057600db7cc
tracer-ysyx2204 <tracer@ysyx.org>
2023-10-12 19:17:27 +0800

    > compile NEMU ysyx_22040905 程雨航 Linux cyh-RedmiBook-Pro-14S 5.15.0-25-generic #25-Ubuntu SMP Wed Mar 30 15:54:22 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  19:17:27 up  3:17,  2 users,  load average: 0.90, 1.03, 0.76

--- nanos-lite/src/main.c
+++ nanos-lite/src/main.c
@@ -8,7 +8,6 @@ void init_irq(void);
 void init_fs(void);
 void init_proc(void);
 int main() {
-	while(1);
   extern const char logo[];
   printf("%s", logo);
   Log("'Hello World!' from Nanos-lite");

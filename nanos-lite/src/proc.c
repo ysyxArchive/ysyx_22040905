ff7d416c304a444136e64461045d2021d34132e5
tracer-ysyx2204 <tracer@ysyx.org>
2023-10-13 16:36:02 +0800

    > compile NEMU ysyx_22040905 程雨航 Linux cyh-RedmiBook-Pro-14S 5.15.0-25-generic #25-Ubuntu SMP Wed Mar 30 15:54:22 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  16:36:02 up 1 day, 36 min,  5 users,  load average: 2.54, 1.89, 1.23

--- nanos-lite/src/proc.c
+++ nanos-lite/src/proc.c
@@ -26,7 +26,7 @@ void init_proc() {
 
   Log("Initializing processes...");
 
-  int choose = 6;
+  int choose = 7;
   switch(choose){
     case 0: naive_uload(NULL,"/bin/pal"); break;
     case 1: naive_uload(NULL,"/bin/menu"); break;

6ee597176b7bc7c32ef24407f89cec9a554ac348
tracer-ysyx2204 <tracer@ysyx.org>
2023-10-13 18:21:22 +0800

    > compile NEMU ysyx_22040905 程雨航 Linux cyh-RedmiBook-Pro-14S 5.15.0-25-generic #25-Ubuntu SMP Wed Mar 30 15:54:22 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  18:21:22 up 1 day,  2:21,  5 users,  load average: 1.24, 1.23, 1.24

--- navy-apps/libs/libminiSDL/src/video.c
+++ navy-apps/libs/libminiSDL/src/video.c
@@ -54,10 +54,11 @@ void SDL_UpdateRect(SDL_Surface *s, int x, int y, int w, int h) {
   if(s->format->BitsPerPixel==8){
     for(int i=0;i<h;i++)
       for(int j=0;j<w;j++){
-      uint32_t r=(s->format->palette->colors)[s->pixels[(i)*(s->w)+j]].r;
-      uint32_t g=(s->format->palette->colors)[s->pixels[(i)*(s->w)+j]].g;
-      uint32_t b=(s->format->palette->colors)[s->pixels[(i)*(s->w)+j]].b;
-      uint32_t a=(s->format->palette->colors)[s->pixels[(i)*(s->w)+j]].a;
+      uint32_t r=(s->format->palette->colors)[s->pixels[(i+y)*(s->w)+j+x]].r;
+      uint32_t g=(s->format->palette->colors)[s->pixels[(i+y)*(s->w)+j+x]].g;
+      uint32_t b=(s->format->palette->colors)[s->pixels[(i+y)*(s->w)+j+x]].b;
+      uint32_t a=(s->format->palette->colors)[s->pixels[(i+y)*(s->w)+j+x]].a;
+      printf("%d %d %d %d\n",r,g,b,a);
       *(pixels8+(i+y)*w+x+j)=(a<<24)|(r<<16)|(g<<8)|b;
     }
     NDL_DrawRect(pixels8,x,y,w,h);

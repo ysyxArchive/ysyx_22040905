b1da5d8bb296ba62dd9f8bdcd0fb0d34cadb0bee
tracer-ysyx2204 <tracer@ysyx.org>
2023-10-13 09:49:02 +0800

    > compile NEMU ysyx_22040905 程雨航 Linux cyh-RedmiBook-Pro-14S 5.15.0-25-generic #25-Ubuntu SMP Wed Mar 30 15:54:22 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  09:49:02 up 17:49,  5 users,  load average: 0.98, 1.10, 0.93

--- nemu/src/cpu/cpu-exec.c
+++ nemu/src/cpu/cpu-exec.c
@@ -111,7 +111,7 @@ void cpu_exec(uint64_t n) {
   {
     case  NEMU_ABORT: case NEMU_QUIT:
       iringbuf_print(); break;
-    default: break;
+    default: printf("%d\n",nemu_state.state); break;
   }
   switch (nemu_state.state) {
     case NEMU_RUNNING: nemu_state.state = NEMU_STOP; break;

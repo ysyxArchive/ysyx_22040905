2a62f880953c08205d20ddd2d0747c78f722ca62
tracer-ysyx2204 <tracer@ysyx.org>
2023-10-13 18:23:26 +0800

    > compile NEMU ysyx_22040905 程雨航 Linux cyh-RedmiBook-Pro-14S 5.15.0-25-generic #25-Ubuntu SMP Wed Mar 30 15:54:22 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  18:23:26 up 1 day,  2:23,  5 users,  load average: 1.75, 1.55, 1.37

--- navy-apps/libs/libminiSDL/src/video.c
+++ navy-apps/libs/libminiSDL/src/video.c
@@ -59,7 +59,7 @@ void SDL_UpdateRect(SDL_Surface *s, int x, int y, int w, int h) {
       uint32_t b=(s->format->palette->colors)[s->pixels[(i+y)*(s->w)+j+x]].b;
       uint32_t a=(s->format->palette->colors)[s->pixels[(i+y)*(s->w)+j+x]].a;
       *(pixels8+(i+y)*w+x+j)=(a<<24)|(r<<16)|(g<<8)|b;
-      printf("%d\n",(i+y)*w+x+j);
+      printf("%d %d %d %d %d %d\n",x,y,w,h,i,j);
     }
     NDL_DrawRect(pixels8,x,y,w,h);
   }
